81c9c6d8fe1db33678d9b92423ccfde7360393c3
Jérôme Piquot <jpiquot@fiveforty.fr>
2022-11-20 14:58:41 +0100

    add first package

---
+++ src/Core/Common/Hexalith.Extensions/UniqueIdHelper.cs.cs
@@ -0,0 +1,13 @@
+﻿// Fiveforty S.A. Paris France (2022)
+namespace Hexalith.Extensions;
+
+public class UniqueIdHelper
+{
+	public static string UniqueStringId()
+	{
+		return Convert
+				.ToBase64String(Guid.NewGuid().ToByteArray())[..22]
+				.Replace("/", "_", StringComparison.InvariantCulture)
+				.Replace("+", "-", StringComparison.InvariantCulture);
+	}
+}
