f1f2b074879cb26b7cbb1bf4dad0aaef7deb97f1
Jérôme Piquot <jpiquot@fiveforty.fr>
2022-11-21 14:41:12 +0100

    add d365 odata element base class

---
+++ src/Core/Infrastructure/Hexalith.Infrastructure.Dynamics365FinanceAndOperations/Models/ODataElement.cs
@@ -0,0 +1,20 @@
+﻿// Fiveforty S.A. Paris France (2022)
+
+namespace Hexalith.Infrastructure.Dynamics365FinanceAndOperations.Models;
+
+using System.Text.Json.Serialization;
+
+public abstract record ODataElement
+{
+	[JsonConstructor]
+	protected ODataElement(string etag, string dataAreaId)
+	{
+		Etag = etag;
+		DataAreaId = dataAreaId;
+	}
+	[JsonPropertyName("@odata.etag")]
+	public string Etag { get; }
+
+	[JsonPropertyName("dataAreaId")]
+	public string DataAreaId { get; }
+}
