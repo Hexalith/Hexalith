eacccfe9baeb730f3551e13194a70c3e46128452
Jérôme Piquot <jpiquot@fiveforty.fr>
2022-11-24 12:25:41 +0100

    add array helper

---
+++ src/Core/Common/Hexalith.Extensions/Common/ArrayHelper.cs
@@ -0,0 +1,13 @@
+﻿// Fiveforty S.A. Paris France (2022)
+namespace Hexalith.Extensions.Common;
+
+public static class ArrayHelper
+{
+	/// <summary>
+	/// Creates an array containing the object.
+	/// </summary>
+	/// <typeparam name="T">Type of the object</typeparam>
+	/// <param name="obj">Instance of the object</param>
+	/// <returns>An array containing the object</returns>
+	public static T[] IntoArray<T>(this T obj) => new T[] { obj };
+}
---
+++ test/Hexalith.Tests/Core/Common/Extensions/ArrayHelperTest.cs
@@ -0,0 +1,28 @@
+﻿// Fiveforty S.A. Paris France (2022)
+namespace Hexalith.Tests.Core.Common.Extensions;
+
+using FluentAssertions;
+
+using Hexalith.Extensions.Common;
+
+public class ArrayHelperTest
+{
+	[Fact]
+	public void Interger_into_array_should_contain_value() => Into_array_should_contain_value(101);
+
+	[Fact]
+	public void List_into_array_should_contain_integer_value() => Into_array_should_contain_value(new List<string>() { "Hello once", "Hello again " });
+
+	[Fact]
+	public void Object_into_array_should_contain_value() => Into_array_should_contain_value(new { Hello = "Hello", Count = 100 });
+
+	[Fact]
+	public void String_into_array_should_contain_value() => Into_array_should_contain_value("Hello world!");
+
+	private void Into_array_should_contain_value<T>(T value)
+	{
+		T[] array = value.IntoArray();
+		_ = array.Should().ContainSingle();
+		_ = array[0].Should().Be(value);
+	}
+}
