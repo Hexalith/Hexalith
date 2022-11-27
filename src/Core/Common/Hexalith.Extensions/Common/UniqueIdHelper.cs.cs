739847e2fa2204dd627fce1023eceb224df25704
Jérôme Piquot <jpiquot@fiveforty.fr>
2022-11-27 14:50:19 +0100

    fix incorrect project name

--- src/Core/Common/Hexalith.Extensions/Common/UniqueIdHelper.cs.cs
+++ src/Core/Common/Hexalith.Extensions/Common/UniqueIdHelper.cs.cs
@@ -7,36 +7,36 @@ namespace Hexalith.Extensions.Common;
 /// </summary>
 public class UniqueIdHelper
 {
-    private static readonly object _lock = new();
-    private static string? _previousDateTimeId;
+	private static readonly object _lock = new();
+	private static string? _previousDateTimeId;
 
-    /// <summary>
-    /// Generate a new unique id of 17 chararacters from the current date time (yyyyMMddHHmmssfff). Only one Id per millisecond can be generated.
-    /// </summary>
-    /// <returns>Id string</returns>
-    public static string GenerateDateTimeId()
-    {
-        lock (_lock)
-        {
-            string value = DateTime.Now.ToString("yyyyMMddHHmmssfff");
-            while (value == _previousDateTimeId)
-            {
-                Thread.Sleep(1);
-                value = DateTime.Now.ToString("yyyyMMddHHmmssfff");
-            }
-            return _previousDateTimeId = value;
-        }
-    }
+	/// <summary>
+	/// Generate a new unique id of 17 characters from the current date time (yyyyMMddHHmmssfff). Only one Id per millisecond can be generated.
+	/// </summary>
+	/// <returns>Id string</returns>
+	public static string GenerateDateTimeId()
+	{
+		lock (_lock)
+		{
+			string value = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+			while (value == _previousDateTimeId)
+			{
+				Thread.Sleep(1);
+				value = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+			}
+			return _previousDateTimeId = value;
+		}
+	}
 
-    /// <summary>
-    /// Generate a new unique id of 22 chararacters
-    /// </summary>
-    /// <returns>Id string</returns>
-    public static string GenerateUniqueStringId()
-    {
-        return Convert
-                .ToBase64String(Guid.NewGuid().ToByteArray())[..22]
-                .Replace("/", "_", StringComparison.InvariantCulture)
-                .Replace("+", "-", StringComparison.InvariantCulture);
-    }
+	/// <summary>
+	/// Generate a new unique id of 22 characters
+	/// </summary>
+	/// <returns>Id string</returns>
+	public static string GenerateUniqueStringId()
+	{
+		return Convert
+				.ToBase64String(Guid.NewGuid().ToByteArray())[..22]
+				.Replace("/", "_", StringComparison.InvariantCulture)
+				.Replace("+", "-", StringComparison.InvariantCulture);
+	}
 }
