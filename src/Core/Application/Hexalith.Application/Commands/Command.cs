8a995f30761713080d1151090b6df6f498ec5e16
Jérôme Piquot <jpiquot@fiveforty.fr>
2022-12-21 13:48:59 +0100

    add command

---
+++ src/Core/Application/Hexalith.Application/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Command.cs" company="Fiveforty SAS Paris France">
+//     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
+//     Licensed under the MIT license.
+//     See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Hexalith.Application.Commands;
+
+using Hexalith.Domain.Abstractions.Messages;
+
+using System.Runtime.Serialization;
+
+[DataContract]
+public abstract class Command : BaseMessage, ICommand
+{
+}
