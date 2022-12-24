900c6869c4c73290463a37fec547d36156c087f0
Jérôme Piquot <jpiquot@fiveforty.fr>
2022-12-24 18:37:45 +0100

    exception helper tests

---
+++ test/Hexalith.UnitTests/Core/Common/Helpers/ExceptionHelperTest.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="ExceptionHelperTest.cs" company="Fiveforty SAS Paris France">
+//     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
+//     Licensed under the MIT license.
+//     See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Hexalith.UnitTests.Core.Common.Helpers;
+
+using FluentAssertions;
+
+using Hexalith.Extensions.Helpers;
+
+using System;
+
+public class ExceptionHelperTest
+{
+    [Fact]
+    public void Get_full_message_on_nested_exceptions_should_return_all_messages()
+    {
+        const string msg1 = "Error message 1";
+        const string msg2 = "Error 2";
+        const string msg3 = "Message :\n Error 3";
+        const string msg4 = "Hello 4";
+        Exception ex = new(
+            msg1,
+            new Exception(
+                msg2,
+                new Exception(
+                    msg3,
+                    new Exception(msg4))));
+        string message = ex.FullMessage();
+        _ = message.Should().Be($"{msg1}\n{msg2}\n{msg3}\n{msg4}");
+    }
+}
