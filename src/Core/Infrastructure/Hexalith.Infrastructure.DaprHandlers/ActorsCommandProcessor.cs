67a91113926f883d68db62077565c32a7a280e5e
Jérôme Piquot <jpiquot@fiveforty.fr>
2023-01-13 21:16:05 +0100

    update nuget versions

--- src/Core/Infrastructure/Hexalith.Infrastructure.DaprHandlers/ActorsCommandProcessor.cs
+++ src/Core/Infrastructure/Hexalith.Infrastructure.DaprHandlers/ActorsCommandProcessor.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="CommandDispatcher.cs" company="Fiveforty SAS Paris France">
+﻿// <copyright file="ActorsCommandProcessor.cs" company="Fiveforty SAS Paris France">
 //     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
 //     Licensed under the MIT license.
 //     See LICENSE file in the project root for full license information.
