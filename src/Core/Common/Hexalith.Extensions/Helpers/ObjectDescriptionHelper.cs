bd504333f0e602039cc6ba1d622f4f12352e3d7b
Jérôme Piquot <jpiquot@fiveforty.fr>
2023-05-14 16:32:35 +0200

    add object description tests

--- src/Core/Common/Hexalith.Extensions/Helpers/ObjectDescriptionHelper.cs
+++ src/Core/Common/Hexalith.Extensions/Helpers/ObjectDescriptionHelper.cs
@@ -8,6 +8,8 @@
 // ***********************************************************************
 // <copyright file="ObjectDescriptionHelper.cs" company="Fiveforty SAS Paris France">
 //     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
+//     Licensed under the MIT license.
+//     See LICENSE file in the project root for full license information.
 // </copyright>
 // <summary></summary>
 // ***********************************************************************
@@ -33,7 +35,7 @@ public static class ObjectDescriptionHelper
     /// </summary>
     /// <param name="type">The type.</param>
     /// <returns>System.ValueTuple&lt;System.String, System.String, System.String&gt;.</returns>
-    public static (string TypeName, string DisplayName, string Description) DescribeType(this Type type)
+    public static (string TypeName, string DisplayName, string Description) Describe(this Type type)
     {
         string? typeName = null;
         string? name = null;
@@ -79,5 +81,82 @@ public static class ObjectDescriptionHelper
         description ??= name; // If description attributes are not found, use the name.
 
         return (typeName, name, description);
+    }
+
+    /// <summary>
+    /// Describes the specified property.
+    /// </summary>
+    /// <param name="property">The property.</param>
+    /// <returns>System.ValueTuple&lt;System.String, System.String, System.Nullable&lt;System.Object&gt;, System.Boolean&gt;.</returns>
+    public static (string DisplayName, string Description, object? DefaultValue, bool IsRequired) Describe(this PropertyInfo property)
+    {
+        string? name = null;
+        string? description = null;
+        object? defaultValue = property.GetCustomAttribute<DefaultValueAttribute>()?.Value;
+        bool required = property.GetCustomAttribute<RequiredAttribute>() != null;
+
+        // Get the type description from the display attribute.
+        DisplayAttribute? displayAttribute = property.GetCustomAttribute<DisplayAttribute>();
+        if (displayAttribute != null)
+        {
+            name = string.IsNullOrWhiteSpace(displayAttribute.Name) ? null : displayAttribute.Name;
+            description = string.IsNullOrWhiteSpace(displayAttribute.Description) ? null : displayAttribute.Description;
+        }
+
+        if (name == null)
+        {
+            DisplayNameAttribute? displayNameAttribute = property.GetCustomAttribute<DisplayNameAttribute>();
+            if (displayNameAttribute != null)
+            {
+                name = string.IsNullOrWhiteSpace(displayNameAttribute.DisplayName) ? null : displayNameAttribute.DisplayName;
+            }
+        }
+
+        name ??= property.Name.Humanize(); // If name attributes are not found use the type name converted to a sentence.
+
+        if (description == null)
+        {
+            DescriptionAttribute? descriptionAttribute = property.GetCustomAttribute<DescriptionAttribute>();
+            if (descriptionAttribute != null)
+            {
+                description = string.IsNullOrWhiteSpace(descriptionAttribute.Description) ? null : descriptionAttribute.Description;
+            }
+        }
+
+        description ??= name; // If description attributes are not found, use the name.
+
+        return (name, description, defaultValue, required);
+    }
+
+    /// <summary>
+    /// Describes the instance write properties.
+    /// </summary>
+    /// <param name="type">The type.</param>
+    /// <returns>IDictionary&lt;System.String, System.ValueTuple&lt;System.String, System.String, System.Nullable&lt;System.Object&gt;, System.Boolean&gt;&gt;.</returns>
+    public static IDictionary<string, (string DisplayName, string Description, object? DefaultValue, bool IsRequired)> DescribeInstanceWriteProperties(this Type type)
+    {
+        Dictionary<string, (string DisplayName, string Description, object? DefaultValue, bool IsRequired)> result = new();
+        foreach (PropertyInfo property in type.GetProperties(BindingFlags.Instance | BindingFlags.Public).Where(p => p.CanWrite))
+        {
+            result.Add(property.Name, property.Describe());
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Describes the type using attributes.
+    /// </summary>
+    /// <param name="type">The type.</param>
+    /// <returns>System.ValueTuple&lt;System.String, System.String, System.String&gt;.</returns>
+    public static IDictionary<string, (string DisplayName, string Description, object? DefaultValue, bool IsRequired)> DescribeProperties(this Type type)
+    {
+        Dictionary<string, (string DisplayName, string Description, object? DefaultValue, bool IsRequired)> result = new();
+        foreach (PropertyInfo property in type.GetProperties())
+        {
+            result.Add(property.Name, property.Describe());
+        }
+
+        return result;
     }
 }
--- test/Hexalith.UnitTests/Core/Common/Helpers/ObjectDescriptionHelperTest.cs
+++ test/Hexalith.UnitTests/Core/Common/Helpers/ObjectDescriptionHelperTest.cs
@@ -1,8 +1,18 @@
-﻿// <copyright file="ObjectDescriptionHelperTest.cs" company="Fiveforty SAS Paris France">
+﻿// ***********************************************************************
+// Assembly         : Hexalith.UnitTests
+// Author           : Jérôme Piquot
+// Created          : 05-14-2023
+//
+// Last Modified By : Jérôme Piquot
+// Last Modified On : 05-14-2023
+// ***********************************************************************
+// <copyright file="ObjectDescriptionHelperTest.cs" company="Fiveforty SAS Paris France">
 //     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
 //     Licensed under the MIT license.
 //     See LICENSE file in the project root for full license information.
 // </copyright>
+// <summary></summary>
+// ***********************************************************************
 
 namespace Hexalith.UnitTests.Core.Common.Helpers;
 
@@ -14,73 +24,112 @@ using FluentAssertions;
 using Hexalith.Extensions.Helpers;
 using Hexalith.Extensions.Reflections;
 
+/// <summary>
+/// Class ObjectDescriptionHelperTest.
+/// </summary>
 public class ObjectDescriptionHelperTest
 {
+    /// <summary>
+    /// Defines the test method Mappable_type_object_should_return_mapper_value.
+    /// </summary>
     [Fact]
     public void Mappable_type_object_should_return_mapper_value()
     {
-        (string type, string name, string description) = ObjectDescriptionHelper.DescribeType(typeof(MappableTypeTestV2));
+        (string type, string name, string description) = ObjectDescriptionHelper.Describe(typeof(MappableTypeTestV2));
         _ = type.Should().Be("MappableTest");
         _ = name.Should().Be("Mappable test");
         _ = description.Should().Be("Mappable test");
     }
 
+    /// <summary>
+    /// Defines the test method Object_with_description_attribute_should_return_defined_value.
+    /// </summary>
     [Fact]
     public void Object_with_description_attribute_should_return_defined_value()
     {
-        (string type, string name, string description) = ObjectDescriptionHelper.DescribeType(typeof(DescriptionAttributeTest));
+        (string type, string name, string description) = ObjectDescriptionHelper.Describe(typeof(DescriptionAttributeTest));
         _ = type.Should().Be(nameof(DescriptionAttributeTest));
         _ = name.Should().Be("Description attribute test");
         _ = description.Should().Be("This class is used to test a class with a description attribute");
     }
 
+    /// <summary>
+    /// Defines the test method Object_with_display_attribute_should_return_defined_value.
+    /// </summary>
     [Fact]
     public void Object_with_display_attribute_should_return_defined_value()
     {
-        (string type, string name, string description) = ObjectDescriptionHelper.DescribeType(typeof(DisplayAttributeTest));
+        (string type, string name, string description) = ObjectDescriptionHelper.Describe(typeof(DisplayAttributeTest));
         _ = type.Should().Be(nameof(DisplayAttributeTest));
         _ = name.Should().Be("Display attribute example");
         _ = description.Should().Be("Example of using the display attribute to defined name and description");
     }
 
+    /// <summary>
+    /// Defines the test method Object_with_display_name_attribute_should_return_defined_value.
+    /// </summary>
     [Fact]
     public void Object_with_display_name_attribute_should_return_defined_value()
     {
-        (string type, string name, string description) = ObjectDescriptionHelper.DescribeType(typeof(DisplayNameAttributeTest));
+        (string type, string name, string description) = ObjectDescriptionHelper.Describe(typeof(DisplayNameAttributeTest));
         _ = type.Should().Be(nameof(DisplayNameAttributeTest));
         _ = name.Should().Be("Display name attribute example");
         _ = description.Should().Be("Display name attribute example");
     }
 
+    /// <summary>
+    /// Defines the test method Object_with_no_attributes_should_return_type_name.
+    /// </summary>
     [Fact]
     public void Object_with_no_attributes_should_return_type_name()
     {
-        (string type, string name, string description) = ObjectDescriptionHelper.DescribeType(typeof(NoAttributesTest));
+        (string type, string name, string description) = ObjectDescriptionHelper.Describe(typeof(NoAttributesTest));
         _ = type.Should().Be(nameof(NoAttributesTest));
         _ = name.Should().Be("No attributes test");
         _ = description.Should().Be("No attributes test");
     }
 
+    /// <summary>
+    /// Class DescriptionAttributeTest.
+    /// </summary>
     [Description("This class is used to test a class with a description attribute")]
     public class DescriptionAttributeTest
     {
     }
 
+    /// <summary>
+    /// Class DisplayAttributeTest.
+    /// </summary>
     [Display(Name = "Display attribute example", Description = "Example of using the display attribute to defined name and description")]
     public class DisplayAttributeTest
     {
     }
 
+    /// <summary>
+    /// Class DisplayNameAttributeTest.
+    /// </summary>
     [DisplayName("Display name attribute example")]
     public class DisplayNameAttributeTest
     {
     }
 
+    /// <summary>
+    /// Class MappableTypeTestV2.
+    /// Implements the <see cref="IMappableType" />.
+    /// </summary>
+    /// <seealso cref="IMappableType" />
     public class MappableTypeTestV2 : IMappableType
     {
+        /// <summary>
+        /// Gets the name of the type.
+        /// </summary>
+        /// <value>The name of the type.</value>
         public string TypeMapName => "MappableTest";
     }
 
+    /// <summary>
+    /// Class NoAttributesTest.
+    /// </summary>
     public class NoAttributesTest
     {
     }
