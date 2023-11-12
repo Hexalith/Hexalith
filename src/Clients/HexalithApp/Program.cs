2f952b497fa95ed65879acf38bc33bf945754d50
Jérôme Piquot <jpiquot@fiveforty.fr>
2023-11-12 21:48:13 +0100

    add hexalith client app (blazor server)

---
+++ src/Clients/HexalithApp/Program.cs
@@ -0,0 +1,92 @@
+// <copyright file="Program.cs" company="Fiveforty SAS Paris France">
+//     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
+//     Licensed under the MIT license.
+//     See LICENSE file in the project root for full license information.
+// </copyright>
+
+using HexalithApp.Components;
+
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Identity.Web;
+
+using Radzen;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container for Radzen UI.
+builder.Services.AddRadzenComponents();
+
+// Add services to the container.
+builder.Services
+    .AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddMicrosoftIdentityConsentHandler();
+
+// Configuring appsettings section AzureAdB2C, into IOptions
+builder.Services.AddOptions();
+builder.Services.Configure<OpenIdConnectOptions>(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+
+    // Handling SameSite cookie according to https://learn.microsoft.com/aspnet/core/security/samesite?view=aspnetcore-3.1
+    _ = options.HandleSameSiteCookieCompatibility();
+});
+
+builder
+    .Services
+    .AddMicrosoftIdentityWebAppAuthentication(builder.Configuration)
+    .EnableTokenAcquisitionToCallDownstreamApi(["user.read"])
+    .AddMicrosoftGraph()
+    .AddInMemoryTokenCaches();
+
+// builder.Services.AddRazorPages().AddMvcOptions(options =>
+// {
+//    AuthorizationPolicy policy = new AuthorizationPolicyBuilder()
+//                  .RequireAuthenticatedUser()
+//                  .Build();
+//    options.Filters.Add(new AuthorizeFilter(policy));
+// }).AddMicrosoftIdentityUI();
+builder.Services.AddAuthorization(options => options.FallbackPolicy = options.DefaultPolicy);
+
+// builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddSingleton<IEmailSender, NoOpEmailSender>();
+
+// builder.Services
+//    .AddControllersWithViews()
+//    .AddMicrosoftIdentityUI();
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    _ = app.UseExceptionHandler("/Error", createScopeForErrors: true);
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    _ = app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+// Add the Microsoft Identity Web cookie policy
+app.UseCookiePolicy();
+
+app.UseRouting();
+
+// Add the ASP.NET Core authentication service
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+// .AddAdditionalAssemblies(typeof(Error).Assembly);
+app.Run();
--- src/Core/Infrastructure/Hexalith.Infrastructure.Dynamics365Finance.Inventories/InventoryOnHand/WarehouseOnHandFilter.cs
+++ src/Core/Infrastructure/Hexalith.Infrastructure.Dynamics365Finance.Inventories/InventoryOnHand/WarehouseOnHandFilter.cs
@@ -6,23 +6,25 @@
 // Last Modified By : Jérôme Piquot
 // Last Modified On : 09-25-2023
 // ***********************************************************************
-// <copyright file="WarehouseOnHandFilter.cs" company="Fiveforty S.A.S.">
-//     Copyright (c) Fiveforty S.A.S.. All rights reserved.
+// <copyright file="WarehouseOnHandFilter.cs" company="Fiveforty SAS Paris France">
+//     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
+//     Licensed under the MIT license.
+//     See LICENSE file in the project root for full license information.
 // </copyright>
 // <summary></summary>
 // ***********************************************************************
 
-namespace Hexalith.Infrastructure.Dynamics365Finance.Parties.Customers;
+namespace Hexalith.Infrastructure.Dynamics365Finance.Inventories.InventoryOnHand;
 
 using Hexalith.Infrastructure.Dynamics365Finance.Models;
 
 /// <summary>
 /// Class WarehouseOnHandFilter.
 /// Implements the <see cref="PerCompanyFilter" />
-/// Implements the <see cref="System.IEquatable{Hexalith.Infrastructure.Dynamics365Finance.Parties.Customers.WarehouseOnHandFilter}" />
+/// Implements the <see cref="IEquatable{WarehouseOnHandFilter}" />.
 /// </summary>
 /// <seealso cref="PerCompanyFilter" />
-/// <seealso cref="System.IEquatable{Hexalith.Infrastructure.Dynamics365Finance.Parties.Customers.WarehouseOnHandFilter}" />
+/// <seealso cref="IEquatable{WarehouseOnHandFilter}" />
 public record WarehouseOnHandFilter
 (
     string DataAreaId,
