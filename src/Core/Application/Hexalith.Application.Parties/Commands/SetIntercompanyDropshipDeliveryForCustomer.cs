edd9b24f5227861e60236f3d0684f7de7569b5f6
Jérôme Piquot <jpiquot@fiveforty.fr>
2023-11-19 14:02:11 +0100

    Add bus message labels. #71713

---
+++ src/Core/Application/Hexalith.Application.Parties/Commands/SetIntercompanyDropshipDeliveryForCustomer.cs
@@ -0,0 +1,52 @@
+﻿// ***********************************************************************
+// Assembly         : Hexalith.Application.Parties
+// Author           : Jérôme Piquot
+// Created          : 08-21-2023
+//
+// Last Modified By : Jérôme Piquot
+// Last Modified On : 10-03-2023
+// ***********************************************************************
+// <copyright file="SetIntercompanyDropshipDeliveryForCustomer.cs" company="Fiveforty SAS Paris France">
+//     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
+//     Licensed under the MIT license.
+//     See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+namespace Hexalith.Application.Parties.Commands;
+
+using System.Runtime.Serialization;
+
+using Hexalith.Extensions;
+
+/// <summary>
+/// Class SetCustomerIntercompanyDeliveryToDirect.
+/// Implements the <see cref="Application.Commands.CustomerCommand" />.
+/// </summary>
+/// <seealso cref="Application.Commands.CustomerCommand" />
+[DataContract]
+public class SelectIntercompanyDropshipDeliveryForCustomer : CustomerCommand
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SelectIntercompanyDropshipDeliveryForCustomer"/> class.
+    /// </summary>
+    /// <param name="partitionId">The partition identifier.</param>
+    /// <param name="companyId">The company identifier.</param>
+    /// <param name="id">The identifier.</param>
+    public SelectIntercompanyDropshipDeliveryForCustomer(
+        string partitionId,
+        string companyId,
+        string id)
+        : base(partitionId, companyId, id)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SelectIntercompanyDropshipDeliveryForCustomer"/> class.
+    /// </summary>
+    [Obsolete(DefaultLabels.ForSerializationOnly, true)]
+    public SelectIntercompanyDropshipDeliveryForCustomer()
+    {
+    }
+}
