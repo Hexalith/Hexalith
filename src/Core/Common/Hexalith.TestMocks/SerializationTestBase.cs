87b77f2ec51f3760189aa3a143dbef1275f671c6
Jérôme Piquot <jpiquot@fiveforty.fr>
2023-12-04 19:20:43 +0100

    add unit tests #64576

---
+++ src/Core/Common/Hexalith.TestMocks/SerializationTestBase.cs
@@ -0,0 +1,51 @@
+﻿// ***********************************************************************
+// Assembly         : Hexalith.TestMocks
+// Author           : Jérôme Piquot
+// Created          : 12-04-2023
+//
+// Last Modified By : Jérôme Piquot
+// Last Modified On : 12-04-2023
+// ***********************************************************************
+// <copyright file="SerializationTestBase.cs" company="Fiveforty SAS Paris France">
+//     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
+//     Licensed under the MIT license.
+//     See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+namespace Hexalith.TestMocks;
+
+using FluentAssertions;
+
+using Xunit;
+
+/// <summary>
+/// Class SerializationTestBase.
+/// </summary>
+public abstract class SerializationTestBase
+{
+    /// <summary>
+    /// Defines the test method CheckBinarySerialization.
+    /// </summary>
+    [Fact]
+    public void CheckBinarySerialization() => _ = ToSerializeObject().Should().BeBinarySerializable();
+
+    /// <summary>
+    /// Defines the test method CheckDataContractSerialization.
+    /// </summary>
+    [Fact]
+    public void CheckDataContractSerialization() => _ = ToSerializeObject().Should().BeDataContractSerializable();
+
+    /// <summary>
+    /// Defines the test method CheckJsonSerialization.
+    /// </summary>
+    [Fact]
+    public void CheckJsonSerialization() => _ = ToSerializeObject().Should().BeJsonSerializable();
+
+    /// <summary>
+    /// Converts to serialize object.
+    /// </summary>
+    /// <returns>System.Object.</returns>
+    public abstract object ToSerializeObject();
+}
