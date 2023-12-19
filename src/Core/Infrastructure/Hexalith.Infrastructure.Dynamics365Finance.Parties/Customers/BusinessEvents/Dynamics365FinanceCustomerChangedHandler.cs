3acdbcbabb788a8902622755b0f0b79dc541e1e7
Jérôme Piquot <jpiquot@fiveforty.fr>
2023-12-19 13:15:16 +0100

    split erp customer handlers

--- src/Core/Infrastructure/Hexalith.Infrastructure.Dynamics365Finance.Parties/Customers/BusinessEvents/Dynamics365FinanceCustomerChangedHandler.cs
+++ src/Core/Infrastructure/Hexalith.Infrastructure.Dynamics365Finance.Parties/Customers/BusinessEvents/Dynamics365FinanceCustomerChangedHandler.cs
@@ -15,9 +15,17 @@
 // ***********************************************************************
 namespace Hexalith.Infrastructure.Dynamics365Finance.Parties.Customers.BusinessEvents;
 
+using Hexalith.Application.Commands;
+using Hexalith.Application.Events;
+using Hexalith.Application.ExternalSystems.Commands;
 using Hexalith.Application.Organizations.Configurations;
+using Hexalith.Application.Parties.Commands;
 using Hexalith.Application.Parties.Services;
+using Hexalith.Domain.Aggregates;
+using Hexalith.Domain.ValueObjets;
 using Hexalith.Extensions.Common;
+using Hexalith.Extensions.Configuration;
+using Hexalith.Infrastructure.Dynamics365Finance.Parties.Customers.Helpers;
 
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -27,8 +35,15 @@ using Microsoft.Extensions.Options;
 /// Implements the <see cref="Application.Events.IntegrationEventHandler{Customers.Infrastructure.IntegrationEvents.FFYCustomerChangedBusinessEvent}" />.
 /// </summary>
 /// <seealso cref="Application.Events.IntegrationEventHandler{Customers.Infrastructure.IntegrationEvents.FFYCustomerChangedBusinessEvent}" />
-public class Dynamics365FinanceCustomerChangedHandler : Dynamics365FinanceCustomerInformationHandler<Dynamics365FinanceCustomerChanged>
+public partial class Dynamics365FinanceCustomerChangedHandler : IntegrationEventHandler<Dynamics365FinanceCustomerChanged>
 {
+    private readonly ICustomerProjectionService _customerService;
+    private readonly IDateTimeService _dateTimeService;
+    private readonly ILogger<Dynamics365FinanceCustomerChangedHandler> _logger;
+    private readonly IOptions<OrganizationSettings> _settings;
+    private string _originId;
+    private string _partitionId;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="Dynamics365FinanceCustomerChangedHandler"/> class.
     /// </summary>
@@ -40,11 +55,126 @@ public class Dynamics365FinanceCustomerChangedHandler : Dynamics365FinanceCustom
         IDateTimeService dateTimeService,
         IOptions<OrganizationSettings> settings,
         ILogger<Dynamics365FinanceCustomerChangedHandler> logger)
-        : base(
-            customerService,
-            dateTimeService,
-            settings,
-            logger)
     {
+        ArgumentNullException.ThrowIfNull(customerService);
+        ArgumentNullException.ThrowIfNull(dateTimeService);
+        ArgumentNullException.ThrowIfNull(settings);
+        ArgumentNullException.ThrowIfNull(logger);
+        SettingsException<OrganizationSettings>.ThrowIfNullOrEmpty(settings.Value.DefaultPartitionId);
+        SettingsException<OrganizationSettings>.ThrowIfNullOrEmpty(settings.Value.DefaultOriginId);
+        _partitionId = settings.Value.DefaultPartitionId;
+        _originId = settings.Value.DefaultOriginId;
+
+        _customerService = customerService;
+        _dateTimeService = dateTimeService;
+        _settings = settings;
+        _logger = logger;
     }
+
+    /// <inheritdoc/>
+    public override async Task<IEnumerable<BaseCommand>> ApplyAsync(Dynamics365FinanceCustomerChanged @event, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(@event);
+        ArgumentNullException.ThrowIfNull(@event.Contact);
+        ArgumentNullException.ThrowIfNull(@event.Contact.PostalAddress);
+        ArgumentException.ThrowIfNullOrEmpty(@event.Account);
+        ArgumentException.ThrowIfNullOrEmpty(@event.Name);
+        ArgumentException.ThrowIfNullOrEmpty(@event.BusinessEventLegalEntity);
+        string? aggregateId = @event
+            .ExternalReferences?
+            .Where(p => p.SystemId == nameof(Hexalith))
+            .FirstOrDefault()?
+            .ExternalId;
+        string originId;
+        string customerId;
+        string companyId;
+        Customer? customer = null;
+        if (string.IsNullOrWhiteSpace(aggregateId))
+        {
+            // Ignore event without Hexalith external code if not a registration event
+            LogMessageWithoutHexalithExternalCode(@event.TypeName, @event.BusinessEventLegalEntity, @event.Account);
+            return [];
+        }
+        else
+        {
+            customer = await _customerService
+                .GetCustomerAsync(aggregateId, cancellationToken)
+                .ConfigureAwait(false);
+            if (customer == null)
+            {
+                string[] parts = aggregateId.Split(Aggregate.Separator, 5);
+                companyId = parts[2];
+                originId = parts[3];
+                customerId = parts[4];
+
+                // TODO : Do a call to the customer aggregate service to get the customer
+                // throw new InvalidOperationException($"Customer '{aggregateId}' not found while handling '{@event.BusinessEventLegalEntity}/{@event.Account}' event.");
+            }
+            else
+            {
+                originId = customer.OriginId;
+                customerId = customer.Id;
+                companyId = customer.CompanyId;
+            }
+        }
+
+        string customerAggregateName = Customer.GetAggregateName();
+        List<BaseCommand> commands = [];
+        if (@event.ExternalReferences != null)
+        {
+            foreach (ExternalReference reference in @event.ExternalReferences)
+            {
+                AddExternalSystemReference mapExternalSystemReference = new(
+                        _partitionId,
+                        @event.BusinessEventLegalEntity,
+                        reference.SystemId,
+                        customerAggregateName,
+                        reference.ExternalId,
+                        aggregateId);
+                commands.Add(mapExternalSystemReference);
+            }
+        }
+
+        commands.Add(new AddExternalSystemReference(
+                _partitionId,
+                @event.BusinessEventLegalEntity.ToUpperInvariant(),
+                _originId,
+                customerAggregateName,
+                @event.Account,
+                aggregateId));
+
+        ChangeCustomerInformation changeCustomer = new(
+                   _partitionId,
+                   companyId,
+                   originId,
+                   customerId,
+                   @event.Name,
+                   CustomerConverter.ToPartyType(@event.PartyType ?? nameof(PartyType.Person)),
+                   @event.Contact,
+                   @event.WarehouseId,
+                   @event.CommissionSalesGroupId,
+                   @event.GroupId,
+                   @event.SalesCurrencyId,
+                   _dateTimeService.UtcNow);
+
+        commands.Add(changeCustomer);
+
+        bool directDelivery = @event.InterCompanyDirectDelivery == "Yes";
+        if (directDelivery)
+        {
+            commands.Add(new SelectIntercompanyDropshipDeliveryForCustomer(_partitionId, companyId, _originId, customerId));
+        }
+        else
+        {
+            commands.Add(new DeselectIntercompanyDropshipDeliveryForCustomer(_partitionId, companyId, _originId, customerId));
+        }
+
+        return commands;
+    }
+
+    [LoggerMessage(
+        EventId = 1,
+        Level = LogLevel.Information,
+        Message = "Event {EventType} for customer '{CompanyId}/{CustomerAccount}' without 'Hexalith' external code, is ignored.")]
+    private partial void LogMessageWithoutHexalithExternalCode(string eventType, string companyId, string customerAccount);
 }
--- src/Core/Infrastructure/Hexalith.Infrastructure.Dynamics365Finance.Parties/Customers/BusinessEvents/Dynamics365FinanceCustomerRegisteredHandler.cs
+++ src/Core/Infrastructure/Hexalith.Infrastructure.Dynamics365Finance.Parties/Customers/BusinessEvents/Dynamics365FinanceCustomerRegisteredHandler.cs
@@ -15,9 +15,17 @@
 // ***********************************************************************
 namespace Hexalith.Infrastructure.Dynamics365Finance.Parties.Customers.BusinessEvents;
 
+using Hexalith.Application.Commands;
+using Hexalith.Application.Events;
+using Hexalith.Application.ExternalSystems.Commands;
 using Hexalith.Application.Organizations.Configurations;
+using Hexalith.Application.Parties.Commands;
 using Hexalith.Application.Parties.Services;
+using Hexalith.Domain.Aggregates;
+using Hexalith.Domain.ValueObjets;
 using Hexalith.Extensions.Common;
+using Hexalith.Extensions.Configuration;
+using Hexalith.Infrastructure.Dynamics365Finance.Parties.Customers.Helpers;
 
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -27,24 +35,118 @@ using Microsoft.Extensions.Options;
 /// Implements the <see cref="Application.Events.IntegrationEventHandler{Bspk.Customers.Infrastructure.IntegrationEvents.FFYCustomerRegisteredBusinessEvent}" />.
 /// </summary>
 /// <seealso cref="Application.Events.IntegrationEventHandler{Bspk.Customers.Infrastructure.IntegrationEvents.FFYCustomerRegisteredBusinessEvent}" />
-public class Dynamics365FinanceCustomerRegisteredHandler : Dynamics365FinanceCustomerInformationHandler<Dynamics365FinanceCustomerRegistered>
+public class Dynamics365FinanceCustomerRegisteredHandler : IntegrationEventHandler<Dynamics365FinanceCustomerRegistered>
 {
+    private readonly ICustomerProjectionService _customerService;
+    private readonly IDateTimeService _dateTimeService;
+    private readonly ILogger<Dynamics365FinanceCustomerRegisteredHandler> _logger;
+    private readonly string _originId;
+    private readonly string _partitionId;
+    private readonly IOptions<OrganizationSettings> _settings;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="Dynamics365FinanceCustomerRegisteredHandler"/> class.
     /// </summary>
     /// <param name="customerService">The customer service.</param>
     /// <param name="dateTimeService">The date time service.</param>
     /// <param name="settings">The settings.</param>
+    /// <param name="logger">The logger.</param>
+    /// <exception cref="System.ArgumentNullException">null.</exception>
     public Dynamics365FinanceCustomerRegisteredHandler(
         ICustomerProjectionService customerService,
         IDateTimeService dateTimeService,
         IOptions<OrganizationSettings> settings,
         ILogger<Dynamics365FinanceCustomerRegisteredHandler> logger)
-        : base(
-            customerService,
-            dateTimeService, /*externalReferenceMapperService,*/
-            settings,
-            logger)
     {
+        ArgumentNullException.ThrowIfNull(customerService);
+        ArgumentNullException.ThrowIfNull(dateTimeService);
+        ArgumentNullException.ThrowIfNull(settings);
+        ArgumentNullException.ThrowIfNull(logger);
+        SettingsException<OrganizationSettings>.ThrowIfNullOrEmpty(settings.Value.DefaultPartitionId);
+        SettingsException<OrganizationSettings>.ThrowIfNullOrEmpty(settings.Value.DefaultOriginId);
+
+        _customerService = customerService;
+        _dateTimeService = dateTimeService;
+        _settings = settings;
+        _logger = logger;
+        _partitionId = settings.Value.DefaultPartitionId;
+        _originId = settings.Value.DefaultOriginId;
+    }
+
+    /// <inheritdoc/>
+    public override Task<IEnumerable<BaseCommand>> ApplyAsync(Dynamics365FinanceCustomerRegistered @event, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(@event);
+        ArgumentNullException.ThrowIfNull(@event.Contact);
+        ArgumentNullException.ThrowIfNull(@event.Contact.PostalAddress);
+        ArgumentException.ThrowIfNullOrEmpty(@event.Account);
+        ArgumentException.ThrowIfNullOrEmpty(@event.Name);
+        ArgumentException.ThrowIfNullOrEmpty(@event.BusinessEventLegalEntity);
+        string? aggregateId = @event
+            .ExternalReferences?
+            .Where(p => p.SystemId == nameof(Hexalith))
+            .FirstOrDefault()?
+            .ExternalId;
+        if (!string.IsNullOrWhiteSpace(aggregateId))
+        {
+            throw new InvalidOperationException($"Registrating customer '{aggregateId}' that already has an external id : {nameof(Hexalith)}='{aggregateId}'");
+        }
+
+        string originId = _originId;
+        string customerId = @event.Account;
+        string partitionId = _partitionId;
+        string companyId = @event.BusinessEventLegalEntity.ToUpperInvariant();
+        aggregateId = Customer.GetAggregateId(_partitionId, companyId, _originId, customerId);
+
+        string customerAggregateName = Customer.GetAggregateName();
+        List<BaseCommand> commands = [];
+        if (@event.ExternalReferences != null)
+        {
+            foreach (ExternalReference reference in @event.ExternalReferences)
+            {
+                AddExternalSystemReference mapExternalSystemReference = new(
+                        _partitionId,
+                        @event.BusinessEventLegalEntity,
+                        reference.SystemId,
+                        customerAggregateName,
+                        reference.ExternalId,
+                        aggregateId);
+                commands.Add(mapExternalSystemReference);
+            }
+        }
+
+        commands.Add(new AddExternalSystemReference(
+                _partitionId,
+                @event.BusinessEventLegalEntity.ToUpperInvariant(),
+                _originId,
+                customerAggregateName,
+                @event.Account,
+                aggregateId));
+
+        commands.Add(new RegisterCustomer(
+                   partitionId,
+                   companyId,
+                   originId,
+                   customerId,
+                   @event.Name,
+                   CustomerConverter.ToPartyType(@event.PartyType ?? nameof(PartyType.Person)),
+                   @event.Contact,
+                   @event.WarehouseId,
+                   @event.CommissionSalesGroupId,
+                   @event.GroupId,
+                   @event.SalesCurrencyId,
+                   _dateTimeService.UtcNow));
+
+        bool directDelivery = @event.InterCompanyDirectDelivery == "Yes";
+        if (directDelivery)
+        {
+            commands.Add(new SelectIntercompanyDropshipDeliveryForCustomer(_partitionId, companyId, _originId, customerId));
+        }
+        else
+        {
+            commands.Add(new DeselectIntercompanyDropshipDeliveryForCustomer(_partitionId, companyId, _originId, customerId));
+        }
+
+        return Task.FromResult<IEnumerable<BaseCommand>>(commands);
     }
 }
