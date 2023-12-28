c5b3b51b5e3b03cdbb3cb7c8e2949536078e5d64
Jérôme Piquot <jpiquot@fiveforty.fr>
2023-12-28 11:26:08 +0100

    fix urvey test compile error

---
+++ test/Hexalith.UnitTests/Core/Domain/Surveys/SurveyAggregateTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Hexalith.UnitTests.Core.Domain.Surveys;
+
+using Hexalith.Domain.Aggregates;
+using Hexalith.Domain.Events;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SurveyAggregateTest
+{
+    [Fact]
+    public void PerDaySurveyShouldReturnDailyAverage()
+    {
+//        SurveyRegistered registered = new SurveyRegistered("FFY", "FRRT", "TEST", "Survey1", "Test survey");
+//        Survey survey = new Survey(registered); 
+    }
+}
