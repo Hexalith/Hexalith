12d423ca5a46b380240dc884fc084a778a033eab
Jérôme Piquot <jpiquot@fiveforty.fr>
2024-01-18 16:05:18 +0100

    add web app initialization helper

--- src/Clients/HexalithApplication/HexalithApplication/Helpers/HexalithWebApi.cs
+++ src/Clients/HexalithApplication/HexalithApplication/Helpers/HexalithWebApi.cs
@@ -1,10 +1,18 @@
-﻿// <copyright file="HexalithWebApi.cs" company="Fiveforty SAS Paris France">
+﻿// ***********************************************************************
+// Assembly         : HexalithApplication
+// Author           : Jérôme Piquot
+// Created          : 01-18-2024
+//
+// Last Modified By : Jérôme Piquot
+// Last Modified On : 01-18-2024
+// ***********************************************************************
+// <copyright file="HexalithWebApi.cs" company="Fiveforty SAS Paris France">
 //     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
-//     Licensed under the MIT license.
-//     See LICENSE file in the project root for full license information.
+// Licensed under the MIT license.
+//    See LICENSE file in the project root for full license information.
 // </copyright>
-
-using Hexalith.Infrastructure.WebApis.Helpers;
+// <summary></summary>
+// ***********************************************************************
 
 namespace HexalithApplication.Helpers;
 
@@ -18,17 +26,30 @@ using FluentValidation;
 using Hexalith.Application.Aggregates;
 using Hexalith.Application.Buses;
 using Hexalith.Application.Commands;
+using Hexalith.Application.Organizations.Helpers;
 using Hexalith.Application.Projection;
 using Hexalith.Application.States;
 using Hexalith.Application.Tasks;
 using Hexalith.Domain.Messages;
 using Hexalith.Extensions.Common;
 using Hexalith.Infrastructure.DaprRuntime.Helpers;
+using Hexalith.Infrastructure.Emails.SendGrid.Helpers;
+using Hexalith.Infrastructure.WebApis.Helpers;
+
+using HexalithApplication.Components;
+using HexalithApplication.Components.Account;
+using HexalithApplication.Data;
+using HexalithApplication.Services;
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 using Serilog;
 
@@ -62,9 +83,10 @@ public static class HexalithWebApplicationHelper
         builder.Services
             .AddEndpointsApiExplorer()
             .AddSwaggerGen(c => c.SwaggerDoc("v1", new() { Title = applicationName, Version = version, }))
-        .AddDaprBuses(builder.Configuration)
-        .AddDaprStateStore(builder.Configuration)
-        .AddActors(options =>
+            .AddOrganizations(builder.Configuration)
+            .AddDaprBuses(builder.Configuration)
+            .AddDaprStateStore(builder.Configuration)
+            .AddActors(options =>
 
             // Register actor types and configure actor settings
             registerActors(options.Actors));
@@ -76,7 +98,68 @@ public static class HexalithWebApplicationHelper
             .AddApplicationPart(typeof(BaseMessage).Assembly) // Issue with MapControllers() throwing a type not found exception for BaseCommand
             .AddDapr();
 
-        if (debugInVisualStudio == true)
+        // Add email services for sending authentication emails.
+        _ = builder.Services
+            .AddSendGridEmail(builder.Configuration)
+            .AddSingleton<IEmailSender, EmailSender>();
+        // Add services to the container.
+        _ = builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+
+        _ = builder.Services
+            .AddHttpClient()
+            .AddFluentUIComponents()
+            .AddCascadingAuthenticationState()
+            .AddScoped<IdentityUserAccessor>()
+            .AddScoped<IdentityRedirectManager>()
+            .AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>()
+            .AddSingleton<IEmailSender<ApplicationUser>, IdentityEmailSender>()
+            .AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+            .AddBearerToken(IdentityConstants.BearerScheme)
+            .AddMicrosoftAccount(microsoftOptions =>
+            {
+                string? clientId = builder.Configuration["Authentication:Microsoft:ClientId"];
+                string? clientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"];
+                if (string.IsNullOrWhiteSpace(clientId))
+                {
+                    throw new InvalidOperationException("Authentication:Microsoft:ClientId must be set in the application settings.");
+                }
+
+                if (string.IsNullOrWhiteSpace(clientSecret))
+                {
+                    throw new InvalidOperationException("Authentication:Microsoft:ClientSecret must be set in the application settings.");
+                }
+
+                microsoftOptions.ClientId = clientId;
+                microsoftOptions.ClientSecret = clientSecret;
+            })
+            .AddIdentityCookies();
+
+        _ = builder.Services
+            .AddAuthorizationBuilder()
+            .AddPolicy("api", p =>
+            {
+                _ = p
+                    .RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes(IdentityConstants.BearerScheme);
+            });
+
+        string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        _ = builder.Services
+            .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString))
+            .AddDatabaseDeveloperPageExceptionFilter()
+            .AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddSignInManager()
+            .AddDefaultTokenProviders()
+            .AddApiEndpoints();
+
+        if (debugInVisualStudio)
         {
             // When debugging, we want to be able to run the application inside Visual Studio to see the technical details.
             _ = builder.Services.AddDaprSidekick(builder.Configuration);
@@ -97,8 +180,8 @@ public static class HexalithWebApplicationHelper
     /// </summary>
     /// <param name="app">The application.</param>
     /// <returns>IApplicationBuilder.</returns>
-    /// <exception cref="ArgumentNullException">null.</exception>
-    public static IApplicationBuilder UseHexalith([NotNull] this WebApplication app)
+    /// <exception cref="System.ArgumentNullException">null</exception>
+    public static IApplicationBuilder UseHexalithWebApplication([NotNull] this WebApplication app)
     {
         ArgumentNullException.ThrowIfNull(app);
         _ = app
@@ -112,15 +195,32 @@ public static class HexalithWebApplicationHelper
 
         if (app.Environment.IsDevelopment())
         {
-            _ = app.UseDeveloperExceptionPage();
+            app
+                .UseDeveloperExceptionPage()
+                .UseMigrationsEndPoint()
+                .UseWebAssemblyDebugging();
         }
-
-        _ = app.UseSwagger();
-        _ = app.UseSwaggerUI();
-
-        _ = app.UseRouting();
-
+        _ = app
+            .UseStaticFiles()
+            .UseAuthentication()
+            .UseAuthorization()
+            .UseSwagger()
+            .UseSwaggerUI()
+            .UseRouting()
+            .UseAntiforgery();
         _ = app.MapActorsHandlers();
+
+        _ = app
+            .MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddAdditionalAssemblies(typeof(HexalithApplication.Client._Imports).Assembly);
+
+        // Add additional endpoints required by the Identity /Account Razor components.
+        _ = app.MapAdditionalIdentityEndpoints();
+
+        _ = app.MapGroup("api/auth").MapIdentityApi<ApplicationUser>();
+
         return app;
     }
 }
--- src/Clients/HexalithApplication/HexalithApplication/Program.cs
+++ src/Clients/HexalithApplication/HexalithApplication/Program.cs
@@ -4,22 +4,10 @@
 //     See LICENSE file in the project root for full license information.
 // </copyright>
 
-using Hexalith.Application.Organizations.Configurations;
-using Hexalith.Extensions.Configuration;
-using Hexalith.Infrastructure.Emails.SendGrid.Helpers;
-using Hexalith.Infrastructure.WebApis.Helpers;
-
 using HexalithApplication.Components;
 using HexalithApplication.Components.Account;
 using HexalithApplication.Data;
 using HexalithApplication.Helpers;
-using HexalithApplication.Services;
-
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.FluentUI.AspNetCore.Components;
 
 using Serilog;
 
@@ -31,99 +19,15 @@ bool debugInVisualStudio = true;
 bool debugInVisualStudio = false;
 #endif
 
-WebApplicationBuilder builder = HexalithWebApi.CreateApplication(
+WebApplicationBuilder builder = HexalithWebApplicationHelper.CreateWebApplication(
     appName,
     "v1",
     debugInVisualStudio,
     (actors) => { },
     args);
 
-// Add email services for sending authentication emails.
-builder.Services.AddSendGridEmail(builder.Configuration);
-builder.Services.AddSingleton<IEmailSender, EmailSender>();
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
-builder.Services.AddHttpClient();
-builder.Services.AddFluentUIComponents();
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddScoped<IdentityUserAccessor>();
-builder.Services.AddScoped<IdentityRedirectManager>();
-builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
-builder.Services.ConfigureSettings<OrganizationSettings>(builder.Configuration);
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
-    .AddBearerToken(IdentityConstants.BearerScheme)
-    .AddMicrosoftAccount(microsoftOptions =>
-    {
-        string? clientId = builder.Configuration["Authentication:Microsoft:ClientId"];
-        string? clientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"];
-        if (string.IsNullOrWhiteSpace(clientId))
-        {
-            throw new InvalidOperationException("Authentication:Microsoft:ClientId must be set in the application settings.");
-        }
-
-        if (string.IsNullOrWhiteSpace(clientSecret))
-        {
-            throw new InvalidOperationException("Authentication:Microsoft:ClientSecret must be set in the application settings.");
-        }
-
-        microsoftOptions.ClientId = clientId;
-        microsoftOptions.ClientSecret = clientSecret;
-    })
-    .AddIdentityCookies();
-
-builder.Services.AddAuthorizationBuilder()
-    .AddPolicy("api", p =>
-    {
-        p.RequireAuthenticatedUser();
-        p.AddAuthenticationSchemes(IdentityConstants.BearerScheme);
-    });
-
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders()
-    .AddApiEndpoints();
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityEmailSender>();
-
 WebApplication app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-    _ = app.UseMigrationsEndPoint();
-}
-
-app.UseStaticFiles();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseAntiforgery();
-
-app.UseHexalith();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(HexalithApplication.Client._Imports).Assembly);
-
-// Add additional endpoints required by the Identity /Account Razor components.
-app.MapAdditionalIdentityEndpoints();
-
-app.MapGroup("api/auth").MapIdentityApi<ApplicationUser>();
+app.UseHexalithWebApplication();
 
 Log.Logger.Information("Starting {AppName}.", appName);
 
