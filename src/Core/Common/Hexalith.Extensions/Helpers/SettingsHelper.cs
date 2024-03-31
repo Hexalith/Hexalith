9a67636fd100bd4ad3bb5f2088011e38b5be0027
Jérôme Piquot <jpiquot@fiveforty.fr>
2024-03-31 14:35:08 +0200

    add settings helper

---
+++ src/Core/Common/Hexalith.Extensions/Helpers/SettingsHelper.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="SettingsHelper.cs" company="Fiveforty SAS Paris France">
+//     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
+//     Licensed under the MIT license.
+//     See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Hexalith.Extensions.Helpers;
+
+using Hexalith.Extensions.Configuration;
+
+using Microsoft.Extensions.Configuration;
+
+/// <summary>
+/// Settings helper class.
+/// </summary>
+public static class SettingsHelper
+{
+    /// <summary>
+    /// Gets the settings from the configuration.
+    /// </summary>
+    /// <typeparam name="TSettings">The type of the settings.</typeparam>
+    /// <param name="configuration">The configuration.</param>
+    /// <returns>The settings object.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the configuration is null.</exception>
+    public static TSettings? GetSettings<TSettings>(this IConfiguration configuration)
+        where TSettings : ISettings, new()
+    {
+        ArgumentNullException.ThrowIfNull(configuration);
+        return configuration.GetSection(TSettings.ConfigurationName()).Get<TSettings>();
+    }
+}
