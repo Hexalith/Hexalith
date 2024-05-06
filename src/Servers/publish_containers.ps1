3705489d1a7ea903bf8e01bca92df60f02683059
Jérôme Piquot <jpiquot@fiveforty.fr>
2024-05-06 15:22:33 +0200

    remove missing project

--- src/Servers/publish_containers.ps1
+++ src/Servers/publish_containers.ps1
@@ -1,8 +1,6 @@
 az login --tenant christofle.com
 az acr login -n christofle
 $version=(minver -t v -v e -d preview)
-cd ./Hexalith.Server.Dynamics365Finance
-dotnet publish /t:PublishContainer -c Release -p ContainerRegistry="christofle.azurecr.io" -p ContainerImageTag=$version
 cd ../Hexalith.Server.ExternalSystems
 dotnet publish /t:PublishContainer -c Release -p ContainerRegistry="christofle.azurecr.io" -p ContainerImageTag=$version
 cd ../Hexalith.Server.Parties
