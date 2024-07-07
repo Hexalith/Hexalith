ddcd35bf501b8d8045a81c69f8c4a82d25894141
Jérôme Piquot <jpiquot@fiveforty.fr>
2024-07-07 12:57:17 +0200

    add polymorphic resolver

--- src/Core/Common/Hexalith.Extensions/Serialization/JsonPolymorphicDerivedTypeAttribute.cs
+++ src/Core/Common/Hexalith.Extensions/Serialization/JsonPolymorphicDerivedTypeAttribute.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="JsonPolymorphicTypeAttribute.cs" company="Fiveforty SAS Paris France">
+﻿// <copyright file="JsonPolymorphicDerivedTypeAttribute.cs" company="Fiveforty SAS Paris France">
 //     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
 //     Licensed under the MIT license.
 //     See LICENSE file in the project root for full license information.
@@ -14,26 +14,26 @@ using System.Reflection;
 /// Attribute to mark a class as a polymorphic type.
 /// </summary>
 /// <remarks>
-/// Initializes a new instance of the <see cref="JsonPolymorphicTypeAttribute"/> class.
+/// Initializes a new instance of the <see cref="JsonPolymorphicDerivedTypeAttribute"/> class.
 /// </remarks>
 /// <param name="typeName">The name of the type.</param>
 /// <param name="version">The version number.</param>
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
-public sealed class JsonPolymorphicTypeAttribute(string typeName, int version) : Attribute
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public sealed class JsonPolymorphicDerivedTypeAttribute(string typeName, int version) : Attribute
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="JsonPolymorphicTypeAttribute"/> class.
+    /// Initializes a new instance of the <see cref="JsonPolymorphicDerivedTypeAttribute"/> class.
     /// </summary>
     /// <param name="typeName">The name of the type.</param>
-    public JsonPolymorphicTypeAttribute(string typeName)
+    public JsonPolymorphicDerivedTypeAttribute(string typeName)
         : this(typeName, 1)
     {
     }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="JsonPolymorphicTypeAttribute"/> class.
+    /// Initializes a new instance of the <see cref="JsonPolymorphicDerivedTypeAttribute"/> class.
     /// </summary>
-    public JsonPolymorphicTypeAttribute()
+    public JsonPolymorphicDerivedTypeAttribute()
         : this(string.Empty, 1)
     {
     }
@@ -47,6 +47,17 @@ public sealed class JsonPolymorphicTypeAttribute(string typeName, int version) :
     /// Gets the version number.
     /// </summary>
     public int Version { get; } = version;
+
+    /// <summary>
+    /// Gets the default type map name.
+    /// </summary>
+    /// <param name="type">Type of the derived instance.</param>
+    /// <returns>The type name.</returns>
+    public static string GetDefaultTypeMapName([NotNull] MemberInfo type)
+    {
+        ArgumentNullException.ThrowIfNull(type);
+        return $"{type.Name}|V1";
+    }
 
     /// <summary>
     /// Gets the type map name.
---
+++ test/Hexalith.UnitTests/Core/Common/Serialization/JsonPolymorphicSerializationTest.cs
@@ -0,0 +1,159 @@
+﻿// <copyright file="JsonPolymorphicSerializationTest.cs" company="Fiveforty SAS Paris France">
+//     Copyright (c) Fiveforty SAS Paris France. All rights reserved.
+//     Licensed under the MIT license.
+//     See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Hexalith.UnitTests.Core.Common.Serialization;
+
+using System.Text.Json;
+
+using FluentAssertions;
+
+using Hexalith.Extensions.Serialization;
+
+public static class JsonPolymorphicSerializationTest
+{
+    private static JsonSerializerOptions Options => new()
+    {
+        WriteIndented = true,
+        TypeInfoResolver = new JsonPolymorphicTypeResolver(new JsonPolymorphicTypeRegistry()),
+    };
+
+    [Fact]
+    public static void Deserialize_polymorphic_no_types_registered_using_base_type_should_throw_an_exception()
+    {
+        // Arrange
+        TestMessage1 message = new("id123", "name123", "value1-123");
+
+        // Act
+        Action serialize = () => _ = JsonSerializer.Serialize(message);
+
+        // Assert
+        _ = serialize
+            .Should()
+            .Throw<InvalidOperationException>()
+            .Where(p => p.Message.Contains(nameof(TestMessageBase)));
+    }
+
+    [Fact]
+    public static void Deserialize_polymorphic_type_not_registered_using_base_type_should_throw_an_exception()
+    {
+        // Arrange
+        TestMessage1 message = new("id123", "name123", "value1-123");
+        JsonPolymorphicTypeRegistry registry = new();
+        registry.RegisterJsonDerivedType<TestMessage2>();
+        JsonSerializerOptions options = new()
+        {
+            WriteIndented = true,
+            TypeInfoResolver = new JsonPolymorphicTypeResolver(registry),
+        };
+
+        // Act
+        Action serialize = () => _ = JsonSerializer.Serialize(message, options);
+
+        // Assert
+        _ = serialize
+            .Should()
+            .Throw<InvalidOperationException>()
+            .Where(p => p.Message.Contains(nameof(TestMessageBase)));
+    }
+
+    [Fact]
+    public static void Serialize_and_deserialize_registered_polymorphic_custom_name_type_not_registered_should_succeed()
+    {
+        // Arrange
+        TestMessageWithCustomName message = new("id123", "name123", "value2-123");
+        JsonPolymorphicTypeRegistry registry = new();
+        registry.RegisterJsonDerivedType<TestMessageWithCustomName>();
+        JsonSerializerOptions options = new()
+        {
+            WriteIndented = true,
+            TypeInfoResolver = new JsonPolymorphicTypeResolver(registry),
+        };
+
+        // Act
+        string json = JsonSerializer.Serialize(message, options);
+        TestMessageWithCustomName result = JsonSerializer.Deserialize<TestMessageWithCustomName>(json, options);
+
+        // Assert
+        _ = result.Should().BeEquivalentTo(message);
+        _ = json.Should().Contain("$type");
+        _ = json.Should().Contain("hello|V99");
+    }
+
+    [Fact]
+    public static void Serialize_and_deserialize_registered_polymorphic_type_not_registered_should_succeed()
+    {
+        // Arrange
+        TestMessage1 message = new("id123", "name123", "value1-123");
+
+        // Act
+        string json = JsonSerializer.Serialize(message, Options);
+        TestMessage1 result = JsonSerializer.Deserialize<TestMessage1>(json, Options);
+
+        // Assert
+        _ = result.Should().BeEquivalentTo(message);
+    }
+
+    [Fact]
+    public static void Serialize_polymorphic_type_not_registered_using_base_type_should_throw_an_exception()
+    {
+        // Arrange
+        TestMessage1 message = new("id123", "name123", "value1-123");
+        string json = """
+            {
+              "$type": "TestMessage1|V1",
+              "Value1": "value1-123",
+              "Id": "id123",
+              "Name": "name123"
+            }
+            """;
+
+        // Act
+        _ = JsonSerializer.Serialize(message, Options);
+
+        // Assert
+        _ = json.Should().NotBeNullOrEmpty();
+        _ = json.Should().NotContain("$type");
+    }
+
+    [Fact]
+    public static void SerializePolymorphicObjectShouldSucceed()
+    {
+        // Arrange
+        TestMessage1 message = new("id123", "name123", "value1-123");
+        JsonPolymorphicTypeRegistry registry = new();
+        registry.RegisterJsonDerivedType<TestMessage1>();
+        JsonSerializerOptions options = new()
+        {
+            WriteIndented = true,
+            TypeInfoResolver = new JsonPolymorphicTypeResolver(registry),
+        };
+
+        // Act
+        string json = JsonSerializer.Serialize(message, options);
+
+        // Assert
+        _ = json.Should().NotBeNullOrEmpty();
+        _ = json.Should().Contain("TestMessage1");
+        _ = json.Should().Contain("id123");
+        _ = json.Should().Contain("name123");
+        _ = json.Should().Contain("value1-123");
+        _ = json.Should().Contain("$type");
+    }
+
+    [Fact]
+    public static void SerializePolymorphicTypeNotRegisteredShouldSerializeWithoutTypeDefinition()
+    {
+        // Arrange
+        TestMessage1 message = new("id123", "name123", "value1-123");
+
+        // Act
+        string json = JsonSerializer.Serialize(message, Options);
+
+        // Assert
+        _ = json.Should().NotBeNullOrEmpty();
+        _ = json.Should().NotContain("$type");
+    }
+}
