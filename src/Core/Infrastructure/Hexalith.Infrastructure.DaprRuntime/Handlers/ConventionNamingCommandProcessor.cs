24f0bf976d235064382ec66cfbee25f5f799d154
Jérôme Piquot <jpiquot@fiveforty.fr>
2024-09-11 09:55:37 +0200

    fix

--- src/Core/Infrastructure/Hexalith.Infrastructure.DaprRuntime/Handlers/ConventionNamingCommandProcessor.cs
+++ src/Core/Infrastructure/Hexalith.Infrastructure.DaprRuntime/Handlers/ConventionNamingCommandProcessor.cs
@@ -1,16 +1,8 @@
-﻿// <copyright file="ConventionNamingCommandProcessor.cs" company="Jérôme Piquot">
-//     Copyright (c) Jérôme Piquot. All rights reserved.
-//     Licensed under the MIT license.
-//     See LICENSE file in the project root for full license information.
+﻿// <copyright file="ConventionNamingCommandProcessor.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
 namespace Hexalith.Infrastructure.DaprRuntime.Handlers;
-
-using Dapr.Actors.Client;
-
-using Hexalith.Application.Commands;
-
-using Microsoft.Extensions.Logging;
 
 /// <summary>
 /// Convention naming command processor.
@@ -24,20 +16,20 @@ using Microsoft.Extensions.Logging;
 /// </remarks>
 /// <param name="actorProxy">The actor proxy.</param>
 /// <param name="logger">The logger.</param>
-[Obsolete("Use AggregateActorCommandProcessor instead.", true)]
-public class ConventionNamingCommandProcessor(IActorProxyFactory actorProxy, ILogger<ConventionNamingCommandProcessor> logger) : ActorsCommandProcessor(actorProxy, logger), IConventionNamingCommandProcessor
-{
-    /// <inheritdoc/>
-    protected override string GetActorMethodName(ICommand command)
-    {
-        ArgumentNullException.ThrowIfNull(command);
-        return "Do" + command.GetType().Name + "Async";
-    }
+// [Obsolete("Use AggregateActorCommandProcessor instead.", true)]
+// public class ConventionNamingCommandProcessor(IActorProxyFactory actorProxy, ILogger<ConventionNamingCommandProcessor> logger) : ActorsCommandProcessor(actorProxy, logger), IConventionNamingCommandProcessor
+// {
+//    /// <inheritdoc/>
+//    protected override string GetActorMethodName(ICommand command)
+//    {
+//        ArgumentNullException.ThrowIfNull(command);
+//        return "Do" + command.GetType().Name + "Async";
+//    }
 
-    /// <inheritdoc/>
-    protected override string GetActorName(ICommand command)
-    {
-        ArgumentNullException.ThrowIfNull(command);
-        return command.AggregateName + "AggregateActor";
-    }
-}
+// /// <inheritdoc/>
+//    protected override string GetActorName(ICommand command)
+//    {
+//        ArgumentNullException.ThrowIfNull(command);
+//        return command.AggregateName + "AggregateActor";
+//    }
+// }
