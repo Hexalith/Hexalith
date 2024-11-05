c442fd76eba30d4c5af14388e6638612e315d20b
Jérôme Piquot <jpiquot@itaneo.com>
2024-11-05 19:03:35 +0100

    Add authentication, security menus, and localization
    - Mark subproject commits for `Hexalith`, `Hexalith.EasyAuthentication`, and `HexalithApp` as dirty.
    - Update `ManholeConstructionSiteSharedModule.cs` to include new dependencies, change priority, and update service registration.
    - Modify `ManholeMenu.cs` to change priority and remove a test menu item.
    - Remove `AuthorizeTest.razor`.
    - Update import in `SharedApplication.cs`.
    - Add `ClaimsView.razor` to display user claims and authentication info.
    - Add `ClaimsIndex.razor` to render `ClaimsView` component.
    - Add `HexalithEasyAuthenticationSharedModule` for Easy Authentication services.
    - Add `SecurityMenu` static class and related resource files for localization.
    - Update `App.razor` with new HTML structure, scripts, and `HandleRedirect` method.

--- src/Core/UI/Hexalith.UI.Components.Abstractions/Icons/IconInformation.cs
+++ src/Core/UI/Hexalith.UI.Components.Abstractions/Icons/IconInformation.cs
@@ -1,7 +1,6 @@
-﻿// <copyright file="IconInformation.cs" company="Jérôme Piquot">
-//     Copyright (c) Jérôme Piquot. All rights reserved.
-//     Licensed under the MIT license.
-//     See LICENSE file in the project root for full license information.
+﻿// <copyright file="IconInformation.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
 namespace Hexalith.UI.Components.Icons;
@@ -15,7 +14,6 @@ using System.Runtime.Serialization;
 /// <param name="Size">The icon size.</param>
 /// <param name="Style">The icon style.</param>
 /// <param name="Source">The icon source.</param>
-/// <param name="Aspect">The icon aspect.</param>
 /// <param name="IconLibraryName">The razor component namespace of the icon library.</param>
 [DataContract]
 public record IconInformation(
@@ -23,7 +21,16 @@ public record IconInformation(
     [property: DataMember] int Size,
     [property: DataMember] IconStyle Style,
     [property: DataMember] IconSource Source,
-    [property: DataMember] string? IconLibraryName
-    )
+    [property: DataMember] string? IconLibraryName)
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IconInformation"/> class.
+    /// </summary>
+    /// <param name="name">The name of the icon.</param>
+    /// <param name="size">The size of the icon.</param>
+    /// <param name="style">The style of the icon.</param>
+    public IconInformation(string name, int size, IconStyle style)
+        : this(name, size, style, IconSource.Fluent, null)
+    {
+    }
 }
