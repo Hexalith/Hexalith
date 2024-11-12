f1d81da07ed39771e4b22c61fa9d80df72dcc8bf
Jérôme Piquot <jpiquot@itaneo.com>
2024-11-12 14:52:11 +0100

    Update target frameworks and package versions
    Updated Hexalith.Extensions.csproj to support multiple target frameworks. Modified version references in various .csproj files to use new version properties. Removed AspNetCore.HealthChecks.Uris package reference. Updated Dependencies.props and Project.Common.props for new versioning and formatting improvements.

--- src/Core/Infrastructure/Hexalith.Infrastructure.AspireService.Defaults/ServiceReferenceExtensions.cs
+++ src/Core/Infrastructure/Hexalith.Infrastructure.AspireService.Defaults/ServiceReferenceExtensions.cs
@@ -99,55 +99,6 @@ public static class ServiceReferenceExtensions
             : services.AddHttpClient<TClient>(c => c.BaseAddress = new(baseAddress));
     }
 
-    /// <summary>
-    /// Adds an HTTP service reference. Configures a binding between the <typeparamref name="TClient"/> type and a named <see cref="HttpClient"/>
-    /// with a base address and health check. The client name will be set to the type name of <typeparamref name="TClient"/>.
-    /// </summary>
-    /// <typeparam name="TClient">
-    /// The type of the typed client. The type specified will be registered in the service collection as a transient service. See
-    /// <see cref="Microsoft.Extensions.Http.ITypedHttpClientFactory{TClient}"/> for more details about authoring typed clients.
-    /// </typeparam>
-    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-    /// <param name="baseAddress">The value to assign to the <see cref="HttpClient.BaseAddress"/> property of the typed client's injected <see cref="HttpClient"/> instance.</param>
-    /// <param name="healthRelativePath">The relative path of the health check endpoint for this HTTP service.</param>
-    /// <param name="healthCheckName">The name for the health check. Defaults to <c>"{typeof(TClient).Name}-health"</c> if not provided.</param>
-    /// <param name="failureStatus">The <see cref="HealthStatus"/> that should be reported if the health check fails. Defaults to <see cref="HealthStatus.Unhealthy"/>.</param>
-    /// <returns>An <see cref="IHttpClientBuilder"/> that can be used to configure the client.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="baseAddress"/> or <paramref name="healthRelativePath"/> are not valid URI values.</exception>
-    public static IHttpClientBuilder AddHttpServiceReference<TClient>(
-        this IServiceCollection services,
-        string baseAddress,
-        string healthRelativePath,
-        string? healthCheckName,
-        HealthStatus failureStatus)
-        where TClient : class
-    {
-        ArgumentNullException.ThrowIfNull(services);
-        ArgumentException.ThrowIfNullOrEmpty(healthRelativePath);
-
-        if (!Uri.IsWellFormedUriString(baseAddress, UriKind.Absolute))
-        {
-            throw new ArgumentException("Base address must be a valid absolute URI.", nameof(baseAddress));
-        }
-
-        if (!Uri.IsWellFormedUriString(healthRelativePath, UriKind.Relative))
-        {
-            throw new ArgumentException("Health check path must be a valid relative URI.", nameof(healthRelativePath));
-        }
-
-        Uri uri = new(baseAddress);
-        IHttpClientBuilder builder = services.AddHttpClient<TClient>(c => c.BaseAddress = uri);
-
-        _ = services.AddHealthChecks()
-            .AddUrlGroup(
-                new Uri(uri, healthRelativePath),
-                healthCheckName ?? $"{typeof(TClient).Name}-health",
-                failureStatus,
-                configurePrimaryHttpMessageHandler: s => s.GetRequiredService<IHttpMessageHandlerFactory>().CreateHandler());
-
-        return builder;
-    }
-
     private static void AddGrpcHealthChecks(IServiceCollection services, Uri uri, string healthCheckName, HealthStatus failureStatus = default)
     {
         _ = services.AddGrpcClient<Health.HealthClient>(o => o.Address = uri);
