e95ffc224710b74c0baf1f27a822456548f51a38
Jérôme Piquot <jpiquot@itaneo.com>
2024-11-24 09:15:39 +0100

    Add new projects and conditional references to solution
    - Updated Hexalith subproject commit to include `-dirty` suffix.
    - Added multiple new projects to Hexalith.DaprIdentityStore.sln.
    - Updated solution configuration and nested projects sections.
    - Added conditional project references in various .csproj files.
    - Removed package reference to Hexalith.UI.Components.
    - Commented out IConversationThreadService.cs file.
    - Commented out specific project references in Hexalith.Application.Abstractions.csproj.

--- src/Core/Application/Hexalith.Application.Abstractions/ConversationThreads/IConversationThreadService.cs
+++ src/Core/Application/Hexalith.Application.Abstractions/ConversationThreads/IConversationThreadService.cs
@@ -3,33 +3,33 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
-namespace Hexalith.Application.ConversationThreads;
+// namespace Hexalith.Application.ConversationThreads;
 
-using System.Threading.Tasks;
+// using System.Threading.Tasks;
 
-using Hexalith.Domain.ConversationThreads.Entities;
+// using Hexalith.Domain.ConversationThreads.Entities;
 
-/// <summary>
-/// Interface IConversationThreadService.
-/// </summary>
-public interface IConversationThreadService
-{
-    /// <summary>
-    /// Adds the message asynchronous.
-    /// </summary>
-    /// <param name="conversationId">The conversation identifier.</param>
-    /// <param name="userId">The user identifier.</param>
-    /// <param name="userName">Name of the user.</param>
-    /// <param name="message">The message.</param>
-    /// <param name="none">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-    /// <returns>Task.</returns>
-    Task AddMessageAsync(string conversationId, string userId, string userName, string message, CancellationToken none);
+///// <summary>
+///// Interface IConversationThreadService.
+///// </summary>
+// public interface IConversationThreadService
+// {
+//    /// <summary>
+//    /// Adds the message asynchronous.
+//    /// </summary>
+//    /// <param name="conversationId">The conversation identifier.</param>
+//    /// <param name="userId">The user identifier.</param>
+//    /// <param name="userName">Name of the user.</param>
+//    /// <param name="message">The message.</param>
+//    /// <param name="none">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+//    /// <returns>Task.</returns>
+//    Task AddMessageAsync(string conversationId, string userId, string userName, string message, CancellationToken none);
 
-    /// <summary>
-    /// Gets the conversation messages asynchronous.
-    /// </summary>
-    /// <param name="id">The identifier.</param>
-    /// <param name="cancellationToken">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-    /// <returns>Task&lt;IEnumerable&lt;ConversationItem&gt;&gt;.</returns>
-    Task<IEnumerable<ConversationItem>> GetConversationMessagesAsync(string id, CancellationToken cancellationToken);
-}
+// /// <summary>
+//    /// Gets the conversation messages asynchronous.
+//    /// </summary>
+//    /// <param name="id">The identifier.</param>
+//    /// <param name="cancellationToken">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+//    /// <returns>Task&lt;IEnumerable&lt;ConversationItem&gt;&gt;.</returns>
+//    Task<IEnumerable<ConversationItem>> GetConversationMessagesAsync(string id, CancellationToken cancellationToken);
+// }
