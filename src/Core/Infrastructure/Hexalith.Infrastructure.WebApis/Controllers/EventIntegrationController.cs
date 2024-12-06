fbe4720d3bcec2b59c3d65f791e6d6ac6deaa09b
Jérôme Piquot <jpiquot@itaneo.com>
2024-12-06 15:36:25 +0100

    Refactor namespaces and add new project for services
    Updated Hexalith subproject commit to include "-dirty" suffix.
    Added new project `Hexalith.Documents.UI.Services` to solution.
    Refactored namespaces and class references across multiple files.
    Removed several interfaces and records related to document and file type services.
    Updated Razor components to use new service and view model namespaces.
    Added new interfaces and view models for document, document type, and file type services.
    Implemented new controllers and handlers for integration events and projections.
    Updated `_Imports.razor` to include new namespaces.

--- src/Core/Infrastructure/Hexalith.Infrastructure.WebApis/Controllers/EventIntegrationController.cs
+++ src/Core/Infrastructure/Hexalith.Infrastructure.WebApis/Controllers/EventIntegrationController.cs
@@ -1,17 +1,8 @@
-﻿// ***********************************************************************
-// Assembly         : Hexalith.Infrastructure.WebApis
-// Author           : JérômePiquot
-// Created          : 01-13-2023
-//
-// Last Modified By : JérômePiquot
-// Last Modified On : 10-26-2023
-// ***********************************************************************
-// <copyright file="EventIntegrationController.cs" company="ITANEO">
+﻿// <copyright file="EventIntegrationController.cs" company="ITANEO">
 // Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
-// <summary></summary>
-// ***********************************************************************
+
 namespace Hexalith.Infrastructure.WebApis.Controllers;
 
 using Hexalith.Application.Events;
