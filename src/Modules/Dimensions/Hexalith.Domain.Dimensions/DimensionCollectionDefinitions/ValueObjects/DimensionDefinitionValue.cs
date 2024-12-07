4851065ff91ef2fdfbebf68d9964a238ab844f19
Jérôme Piquot <jpiquot@itaneo.com>
2024-12-07 13:57:16 +0100

    Update namespaces, align braces, and clean up files
    - Include `Hexalith.PolymorphicSerialization` namespace in `DimensionCollectionDefinitionAdded.cs` and adjust formatting.
    - Correct closing brace alignment in `DimensionCollectionDefinitionEvent.cs`.
    - Update copyright to "ITANEO" and add XML documentation in `DimensionDefinitionValue.cs`.
    - Remove copyright info, `#pragma warning disable SA1200`, and `global using Xunit;` directive from `Usings.cs`.

--- src/Modules/Dimensions/Hexalith.Domain.Dimensions/DimensionCollectionDefinitions/ValueObjects/DimensionDefinitionValue.cs
+++ src/Modules/Dimensions/Hexalith.Domain.Dimensions/DimensionCollectionDefinitions/ValueObjects/DimensionDefinitionValue.cs
@@ -1,7 +1,6 @@
-﻿// <copyright file="DimensionDefinitionValue.cs" company="Jérôme Piquot">
-//     Copyright (c) Jérôme Piquot. All rights reserved.
-//     Licensed under the MIT license.
-//     See LICENSE file in the project root for full license information.
+﻿// <copyright file="DimensionDefinitionValue.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
 namespace Hexalith.Domain.Dimensions.DimensionCollectionDefinitions.ValueObjects;
@@ -11,6 +10,8 @@ using System.Runtime.Serialization;
 /// <summary>
 /// Represents a dimension value.
 /// </summary>
+/// <param name="Id">The unique identifier for the dimension value.</param>
+/// <param name="Name">The name of the dimension value.</param>
 [DataContract]
 public record class DimensionDefinitionValue(
     [property: DataMember(Order = 1)] string Id,
