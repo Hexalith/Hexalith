b0303c185c6c87b5b3664f11f07fa04817fd2983
Jérôme Piquot <jpiquot@itaneo.com>
2024-12-07 11:55:21 +0100

    Add interfaces, records, and handlers for ID collections
    Added copyright headers to `IIdCollectionProjectionFactory.cs`,
    `IdCollection.cs`, and `IdsCollectionProjectionHandler.cs` files,
    indicating ownership by ITANEO and licensing under the MIT license.
    
    Introduced `IIdCollectionProjectionFactory` interface in the
    `Hexalith.Application.Projections` namespace, extending
    `IProjectionFactory<IdCollection>` and including a property
    `CollectionDomainName`.
    
    Added `IdCollection` record type in the
    `Hexalith.Application.Projections` namespace, representing a
    collection of identifiers with properties `NextPageId` and `Ids`,
    both decorated with `DataMember` attributes for serialization.
    
    Implemented `IdsCollectionProjectionHandler<TEvent>` abstract class
    in the `Hexalith.Application.Projections` namespace, handling
    updating projections of ID collections based on events. This class
    includes a constant `_pageSize` set to 1000, an asynchronous method
    `ApplyAsync` to apply events to the projection, and an abstract
    method `IsRemoveEvent` to determine if a given event is a remove
    event.

---
+++ src/Core/Application/Hexalith.Application.Abstractions/Projections/IdCollection.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IdCollection.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Hexalith.Application.Projections;
+
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+/// <summary>
+/// Represents a collection of identifiers.
+/// </summary>
+/// <param name="NextPageId">The next page identifier.</param>
+/// <param name="Ids">The identifiers.</param>
+[DataContract]
+public record IdCollection([property: DataMember(Order = 1)] string? NextPageId, [property: DataMember(Order = 2)] IEnumerable<string> Ids);
---
+++ src/Core/Application/Hexalith.Application/Projections/IdsCollectionProjectionHandler.cs
@@ -0,0 +1,107 @@
+﻿// <copyright file="IdsCollectionProjectionHandler.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Hexalith.Application.Projections;
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Hexalith.Application.Metadatas;
+using Hexalith.Extensions.Helpers;
+
+/// <summary>
+/// Handler for updating projections of ID collections based on events.
+/// </summary>
+/// <typeparam name="TEvent">The type of the event.</typeparam>
+public abstract partial class IdsCollectionProjectionHandler<TEvent>(
+    IProjectionFactory<IdCollection> factory)
+    : IProjectionUpdateHandler<TEvent>
+    where TEvent : class
+{
+    private const int _pageSize = 1000;
+
+    /// <inheritdoc/>
+    public async Task ApplyAsync(TEvent baseEvent, Metadata metadata, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(baseEvent);
+        ArgumentNullException.ThrowIfNull(metadata);
+        IdCollection? currentValue = null;
+        string pageId = metadata.Message.Aggregate.Name;
+        string? freeSpacePageId = null;
+        bool remove = IsRemoveEvent(baseEvent);
+
+        // Loop through the pages to find a page with the current file type ID.
+        do
+        {
+            currentValue = await factory
+            .GetStateAsync(pageId, cancellationToken)
+            .ConfigureAwait(false)
+            ?? new IdCollection(null, []);
+
+            if (currentValue.Ids.Any(p => p == metadata.AggregateGlobalId))
+            {
+                // The file type ID exists in the collection. Remove it if needed.
+                if (remove)
+                {
+                    await factory
+                        .SetStateAsync(
+                            pageId,
+                            currentValue with { Ids = currentValue.Ids.Where(p => p != metadata.AggregateGlobalId) },
+                            cancellationToken)
+                        .ConfigureAwait(false);
+                }
+
+                return;
+            }
+
+            if (currentValue.NextPageId is not null)
+            {
+                pageId = currentValue.NextPageId;
+            }
+
+            if (currentValue.Ids.Count() < _pageSize)
+            {
+                freeSpacePageId = pageId;
+            }
+        }
+        while (currentValue.NextPageId is not null);
+        if (remove)
+        {
+            // The file type ID does not exist in the collection. Nothing to do.
+            return;
+        }
+
+        // The file type ID is not in the collection. Add it.
+        if (freeSpacePageId is null)
+        {
+            // The collection is full. Create a new page.
+            string newPageId = UniqueIdHelper.GenerateUniqueStringId();
+            await factory
+                .SetStateAsync(
+                    pageId,
+                    currentValue with { NextPageId = newPageId, Ids = [metadata.AggregateGlobalId] },
+                    cancellationToken)
+                .ConfigureAwait(false);
+        }
+        else
+        {
+            await factory
+            .SetStateAsync(
+                pageId,
+                currentValue with { Ids = currentValue.Ids.Append(metadata.AggregateGlobalId).Distinct().OrderBy(p => p) },
+                cancellationToken)
+            .ConfigureAwait(false);
+        }
+    }
+
+    /// <summary>
+    /// Determines whether the specified event is a remove event.
+    /// </summary>
+    /// <param name="baseEvent">The event to check.</param>
+    /// <returns><c>true</c> if the specified event is a remove event; otherwise, <c>false</c>.</returns>
+    protected abstract bool IsRemoveEvent(TEvent baseEvent);
+}
