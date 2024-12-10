2ed8bef9f5b3443d4a18d8114054ac47b504a959
Jérôme Piquot <jpiquot@itaneo.com>
2024-12-10 07:46:43 +0100

    Update Hexalith subproject and enhance IRequestHandler docs
    Updated Hexalith subproject commit to include "-dirty" suffix.
    Corrected file name in copyright header of IRequestHandler.cs.
    Added summary documentation to IRequestHandler interface.
    Added XML docs to ExecuteAsync method in IRequestHandler.

--- src/Core/Application/Hexalith.Application.Abstractions/Requests/IRequestHandler.cs
+++ src/Core/Application/Hexalith.Application.Abstractions/Requests/IRequestHandler.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="IIntegrationRequestHandler.cs" company="ITANEO">
+﻿// <copyright file="IRequestHandler.cs" company="ITANEO">
 // Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
@@ -10,7 +10,17 @@ using System.Threading.Tasks;
 
 using Hexalith.Application.Metadatas;
 
+/// <summary>
+/// Defines a handler for processing requests.
+/// </summary>
 public interface IRequestHandler
 {
+    /// <summary>
+    /// Executes the specified request asynchronously.
+    /// </summary>
+    /// <param name="baseRequest">The request to be processed.</param>
+    /// <param name="metadata">The metadata associated with the request.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the response to the request.</returns>
     Task<object> ExecuteAsync(object baseRequest, Metadata metadata, CancellationToken cancellationToken);
 }
