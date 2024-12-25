54095801561e2e093416560f76cf73027d2275b2
Jérôme Piquot <jpiquot@itaneo.com>
2024-12-25 08:11:08 +0100

    Refactor logging in DomainAggregateActorBase
    Removed several logging methods and the Microsoft.Extensions.Logging namespace from DomainAggregateActorBase.cs. Introduced a new file, DomainAggregateActorBase{Log}.cs, to consolidate logging functionality. Updated handling of aggregate IDs and adjusted command processing flow to invoke ProcessNextCommandAsync after saving the state.

--- src/Core/Infrastructure/Hexalith.Infrastructure.DaprRuntime/Actors/DomainAggregateActorBase.cs
+++ src/Core/Infrastructure/Hexalith.Infrastructure.DaprRuntime/Actors/DomainAggregateActorBase.cs
@@ -30,8 +30,6 @@ using Hexalith.Infrastructure.DaprRuntime.Helpers;
 using Hexalith.Infrastructure.DaprRuntime.States;
 using Hexalith.PolymorphicSerialization;
 
-using Microsoft.Extensions.Logging;
-
 /// <summary>
 /// The domain aggregate actor base class.
 /// </summary>
@@ -122,119 +120,6 @@ public abstract partial class DomainAggregateActorBase : Actor, IRemindable, IDo
     private ResiliencyPolicy ResiliencyPolicy
         => _resiliencyPolicy
         ??= _resiliencyPolicyProvider.GetPolicy(Host.ActorTypeInfo.ActorTypeName);
-
-    /// <summary>
-    /// Logs the accepted command information.
-    /// </summary>
-    /// <param name="logger">The logger.</param>
-    /// <param name="commandType">Type of the command.</param>
-    /// <param name="commandId">The command identifier.</param>
-    /// <param name="partitionKey">Key of the aggregate.</param>
-    [LoggerMessage(
-                    EventId = 1,
-                    Level = LogLevel.Information,
-                    Message = "Accepted command '{CommandType}' ({CommandId}) for aggregate key {PartitionKey}.")]
-    public static partial void LogAcceptedCommandInformation(ILogger logger, string commandType, string commandId, string partitionKey);
-
-    /// <summary>
-    /// Logs the invalid publish message error.
-    /// </summary>
-    /// <param name="logger">The logger.</param>
-    /// <param name="sequence">The sequence.</param>
-    /// <param name="messageId">The message identifier.</param>
-    /// <param name="messageType">Type of the message.</param>
-    /// <param name="correlationId">The correlation identifier.</param>
-    /// <param name="aggregateId">The aggregate identifier.</param>
-    [LoggerMessage(
-        EventId = 2,
-        Level = LogLevel.Error,
-        Message = "Message {Sequence} Id={MessageId} Type={MessageType} CorrelationId={CorrelationId} AggregateId={AggregateId} is invalid and cannot be published.")]
-    public static partial void LogInvalidPublishMessageError(
-            ILogger logger,
-            long sequence,
-            string? messageId,
-            string? messageType,
-            string? correlationId,
-            string? aggregateId);
-
-    /// <summary>
-    /// Logs the no commands to submit warning.
-    /// </summary>
-    /// <param name="logger">The logger.</param>
-    /// <param name="actorId">The actor identifier.</param>
-    /// <param name="actorType">Type of the actor.</param>
-    [LoggerMessage(
-                EventId = 3,
-                Level = LogLevel.Warning,
-                Message = "The command envelope submitted to {ActorType} ({ActorId}), has no commands to process.")]
-    public static partial void LogNoCommandsToSubmitWarning(ILogger logger, string actorId, string actorType);
-
-    /// <summary>
-    /// Logs the processed command information.
-    /// </summary>
-    /// <param name="logger">The logger.</param>
-    /// <param name="commandType">Type of the command.</param>
-    /// <param name="commandId">The command identifier.</param>
-    /// <param name="partitionKey"></param>
-    [LoggerMessage(
-                    EventId = 4,
-                    Level = LogLevel.Information,
-                    Message = "Processed command {CommandType} ({CommandId}) for aggregate {PartitionKey}.")]
-    public static partial void LogProcessedCommandInformation(ILogger logger, string commandType, string commandId, string partitionKey);
-
-    /// <summary>
-    /// Logs the processing callback error.
-    /// </summary>
-    /// <param name="logger">The logger.</param>
-    /// <param name="ex">The ex.</param>
-    /// <param name="actorId">The actor identifier.</param>
-    /// <param name="actorType">Type of the actor.</param>
-    /// <param name="commandCount">The command count.</param>
-    /// <param name="commandProcessed">The command processed.</param>
-    [LoggerMessage(
-            EventId = 5,
-            Level = LogLevel.Error,
-            Message = "Actor {ActorType} ({ActorId}) failed processing {CommandProcessed}/{CommandCount} command in a timer or reminder callback. Resetting state.")]
-    public static partial void LogProcessingCallbackError(ILogger logger, Exception ex, string actorId, string actorType, long commandCount, long commandProcessed);
-
-    /// <summary>
-    /// Logs the processing commands information.
-    /// </summary>
-    /// <param name="logger">The logger.</param>
-    /// <param name="actorId">The actor identifier.</param>
-    /// <param name="actorType">Type of the actor.</param>
-    /// <param name="commandCount">The command count.</param>
-    /// <param name="currentCommandProcessed">The current command processed.</param>
-    [LoggerMessage(
-                EventId = 6,
-                Level = LogLevel.Information,
-                Message = "Actor {ActorType} ({ActorId}) is processing command {CurrentCommandProcessed} on a total of {CommandCount}")]
-    public static partial void LogProcessingCommandsInformation(ILogger logger, string actorId, string actorType, long commandCount, long currentCommandProcessed);
-
-    /// <summary>
-    /// Logs the publish error.
-    /// </summary>
-    /// <param name="logger">The logger.</param>
-    /// <param name="exception">The exception.</param>
-    /// <param name="messageSequence">The message sequence.</param>
-    /// <param name="messageId">The message identifier.</param>
-    /// <param name="correlationId">The correlation identifier.</param>
-    /// <param name="actorId">The actor identifier.</param>
-    /// <param name="actorType">Type of the actor.</param>
-    /// <param name="errorMessage">The error message.</param>
-    [LoggerMessage(
-            EventId = 7,
-            Level = LogLevel.Warning,
-            Message = "Publish message {MessageSequence} (Id={MessageId}; CorrelationId={CorrelationId}) operation failed on actor {ActorType}/{ActorId}. Error : {ErrorMessage}")]
-    public static partial void LogPublishError(
-        ILogger logger,
-        Exception exception,
-        long messageSequence,
-        string? messageId,
-        string? correlationId,
-        string actorId,
-        string actorType,
-        string errorMessage);
 
     /// <inheritdoc/>
     public async Task ClearCommandsAsync()
@@ -370,7 +255,7 @@ public abstract partial class DomainAggregateActorBase : Actor, IRemindable, IDo
             throw new InvalidOperationException($"Submitted command to {Host.ActorTypeInfo.ActorTypeName}/{Id} has an invalid aggregate name : {metadata.Message.Aggregate.Name}.");
         }
 
-        if (metadata.AggregateGlobalId != Id.ToString())
+        if (metadata.AggregateGlobalId != Id.ToUnescapeString())
         {
             throw new InvalidOperationException($"Submitted command to {Host.ActorTypeInfo.ActorTypeName}/{Id} has an invalid partition key : {metadata.AggregateGlobalId}.");
         }
@@ -388,13 +273,10 @@ public abstract partial class DomainAggregateActorBase : Actor, IRemindable, IDo
             .AddAsync(commandStates, state.CommandCount, cancellationToken)
             .ConfigureAwait(false);
 
-        await SetProcessCallbackAsync(cancellationToken)
-           .ConfigureAwait(false);
-        await SetPublishCallbackAsync(cancellationToken)
-           .ConfigureAwait(false);
         await SaveAggregateStateAsync(cancellationToken)
             .ConfigureAwait(false);
         await SaveStateAsync().ConfigureAwait(false);
+        _ = await ProcessNextCommandAsync().ConfigureAwait(false);
     }
 
     /// <inheritdoc/>
@@ -405,45 +287,6 @@ public abstract partial class DomainAggregateActorBase : Actor, IRemindable, IDo
         (object command, Metadata metadata) = envelope.Deserialize();
         await SubmitCommandAsStateAsync(new MessageState((PolymorphicRecordBase)command, metadata)).ConfigureAwait(false);
     }
-
-    [LoggerMessage(
-        EventId = 5,
-        Level = LogLevel.Warning,
-        Message = "Application error while processing command number {CommandSequence}, type '{CommandType}', correlationId '{CorrelationId}' for aggregate key '{PartitionKey}' : {ErrorMessage}\n{TechnicalErrorMessage}")]
-    private static partial void LogApplicationErrorWarning(
-        ILogger logger,
-        long commandSequence,
-        string commandType,
-        string partitionKey,
-        string correlationId,
-        string errorMessage,
-        string? technicalErrorMessage);
-
-    [LoggerMessage(
-        EventId = 7,
-        Level = LogLevel.Information,
-        Message = "The command number {Sequence} '{MessageType}' cannot be processed on aggregate key {PartitionKey}. The retry attempt {NextRetryNumber} will be executed in {RetryDueTime} at {NextRetryDateTime}. CorrelationId={CorrelationId}.")]
-    private static partial void LogTaskProcessorRetryInformation(
-        ILogger logger,
-        string messageType,
-        long sequence,
-        string correlationId,
-        string partitionKey,
-        int nextRetryNumber,
-        TimeSpan retryDueTime,
-        DateTimeOffset nextRetryDateTime);
-
-    [LoggerMessage(
-            EventId = 8,
-            Level = LogLevel.Error,
-            Message = "Unhandled command processing error. Command handlers should throw application error exceptions. Processing command number {CommandSequence}, type '{CommandType}', correlationId '{CorrelationId}' for aggregate key '{PartitionKey}'.")]
-    private static partial void LogUnhandledCommandProcessingError(
-        ILogger logger,
-        Exception exception,
-        long commandSequence,
-        string commandType,
-        string partitionKey,
-        string correlationId);
 
     private async Task<IDomainAggregate> GetAggregateAsync(string aggregateName, CancellationToken cancellationToken)
     {
@@ -670,6 +513,7 @@ public abstract partial class DomainAggregateActorBase : Actor, IRemindable, IDo
         }
         catch (ApplicationErrorException ex)
         {
+            _aggregate = null;
             taskProcessor = await HandleCommandProcessingErrorAsync(
                 taskProcessor,
                 commandNumber,
@@ -681,6 +525,7 @@ public abstract partial class DomainAggregateActorBase : Actor, IRemindable, IDo
         }
         catch (Exception e)
         {
+            _aggregate = null;
             LogUnhandledCommandProcessingError(
                 Logger,
                 e,
@@ -775,7 +620,7 @@ public abstract partial class DomainAggregateActorBase : Actor, IRemindable, IDo
     private async Task SetPublishCallbackAsync(CancellationToken cancellationToken)
     {
         AggregateActorState state = await GetAggregateStateAsync(cancellationToken).ConfigureAwait(false);
-        TimeSpan timerWaitTime = timerWaitTime = state.PublishFailed ? _maxTimerDueTime : _defaultTimerDueTime;
+        TimeSpan timerWaitTime = state.PublishFailed ? _maxTimerDueTime : _defaultTimerDueTime;
         TimeSpan reminderWaitTime = _maxTimerDueTime;
 
         if (state.LastMessagePublished < state.MessageCount)
---
+++ src/Core/Infrastructure/Hexalith.Infrastructure.DaprRuntime/Actors/DomainAggregateActorBase{Log}.cs
@@ -0,0 +1,168 @@
+﻿// <copyright file="DomainAggregateActorBase{Log}.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Hexalith.Infrastructure.DaprRuntime.Actors;
+
+using System;
+
+using Microsoft.Extensions.Logging;
+
+/// <summary>
+/// The domain aggregate actor base class.
+/// </summary>
+public abstract partial class DomainAggregateActorBase
+{
+    /// <summary>
+    /// Logs the accepted command information.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    /// <param name="commandType">Type of the command.</param>
+    /// <param name="commandId">The command identifier.</param>
+    /// <param name="partitionKey">Key of the aggregate.</param>
+    [LoggerMessage(
+                    EventId = 1,
+                    Level = LogLevel.Information,
+                    Message = "Accepted command '{CommandType}' ({CommandId}) for aggregate key {PartitionKey}.")]
+    public static partial void LogAcceptedCommandInformation(ILogger logger, string commandType, string commandId, string partitionKey);
+
+    /// <summary>
+    /// Logs the invalid publish message error.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    /// <param name="sequence">The sequence.</param>
+    /// <param name="messageId">The message identifier.</param>
+    /// <param name="messageType">Type of the message.</param>
+    /// <param name="correlationId">The correlation identifier.</param>
+    /// <param name="aggregateId">The aggregate identifier.</param>
+    [LoggerMessage(
+        EventId = 2,
+        Level = LogLevel.Error,
+        Message = "Message {Sequence} Id={MessageId} Type={MessageType} CorrelationId={CorrelationId} AggregateId={AggregateId} is invalid and cannot be published.")]
+    public static partial void LogInvalidPublishMessageError(
+            ILogger logger,
+            long sequence,
+            string? messageId,
+            string? messageType,
+            string? correlationId,
+            string? aggregateId);
+
+    /// <summary>
+    /// Logs the no commands to submit warning.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    /// <param name="actorId">The actor identifier.</param>
+    /// <param name="actorType">Type of the actor.</param>
+    [LoggerMessage(
+                EventId = 3,
+                Level = LogLevel.Warning,
+                Message = "The command envelope submitted to {ActorType} ({ActorId}), has no commands to process.")]
+    public static partial void LogNoCommandsToSubmitWarning(ILogger logger, string actorId, string actorType);
+
+    /// <summary>
+    /// Logs the processed command information.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    /// <param name="commandType">Type of the command.</param>
+    /// <param name="commandId">The command identifier.</param>
+    /// <param name="partitionKey"></param>
+    [LoggerMessage(
+                    EventId = 4,
+                    Level = LogLevel.Information,
+                    Message = "Processed command {CommandType} ({CommandId}) for aggregate {PartitionKey}.")]
+    public static partial void LogProcessedCommandInformation(ILogger logger, string commandType, string commandId, string partitionKey);
+
+    /// <summary>
+    /// Logs the processing callback error.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    /// <param name="ex">The ex.</param>
+    /// <param name="actorId">The actor identifier.</param>
+    /// <param name="actorType">Type of the actor.</param>
+    /// <param name="commandCount">The command count.</param>
+    /// <param name="commandProcessed">The command processed.</param>
+    [LoggerMessage(
+            EventId = 5,
+            Level = LogLevel.Error,
+            Message = "Actor {ActorType} ({ActorId}) failed processing {CommandProcessed}/{CommandCount} command in a timer or reminder callback. Resetting state.")]
+    public static partial void LogProcessingCallbackError(ILogger logger, Exception ex, string actorId, string actorType, long commandCount, long commandProcessed);
+
+    /// <summary>
+    /// Logs the processing commands information.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    /// <param name="actorId">The actor identifier.</param>
+    /// <param name="actorType">Type of the actor.</param>
+    /// <param name="commandCount">The command count.</param>
+    /// <param name="currentCommandProcessed">The current command processed.</param>
+    [LoggerMessage(
+                EventId = 6,
+                Level = LogLevel.Information,
+                Message = "Actor {ActorType} ({ActorId}) is processing command {CurrentCommandProcessed} on a total of {CommandCount}")]
+    public static partial void LogProcessingCommandsInformation(ILogger logger, string actorId, string actorType, long commandCount, long currentCommandProcessed);
+
+    /// <summary>
+    /// Logs the publish error.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    /// <param name="exception">The exception.</param>
+    /// <param name="messageSequence">The message sequence.</param>
+    /// <param name="messageId">The message identifier.</param>
+    /// <param name="correlationId">The correlation identifier.</param>
+    /// <param name="actorId">The actor identifier.</param>
+    /// <param name="actorType">Type of the actor.</param>
+    /// <param name="errorMessage">The error message.</param>
+    [LoggerMessage(
+            EventId = 7,
+            Level = LogLevel.Warning,
+            Message = "Publish message {MessageSequence} (Id={MessageId}; CorrelationId={CorrelationId}) operation failed on actor {ActorType}/{ActorId}. Error : {ErrorMessage}")]
+    public static partial void LogPublishError(
+        ILogger logger,
+        Exception exception,
+        long messageSequence,
+        string? messageId,
+        string? correlationId,
+        string actorId,
+        string actorType,
+        string errorMessage);
+
+    [LoggerMessage(
+        EventId = 8,
+        Level = LogLevel.Warning,
+        Message = "Application error while processing command number {CommandSequence}, type '{CommandType}', correlationId '{CorrelationId}' for aggregate key '{PartitionKey}' : {ErrorMessage}\n{TechnicalErrorMessage}")]
+    private static partial void LogApplicationErrorWarning(
+        ILogger logger,
+        long commandSequence,
+        string commandType,
+        string partitionKey,
+        string correlationId,
+        string errorMessage,
+        string? technicalErrorMessage);
+
+    [LoggerMessage(
+        EventId = 9,
+        Level = LogLevel.Information,
+        Message = "The command number {Sequence} '{MessageType}' cannot be processed on aggregate key {PartitionKey}. The retry attempt {NextRetryNumber} will be executed in {RetryDueTime} at {NextRetryDateTime}. CorrelationId={CorrelationId}.")]
+    private static partial void LogTaskProcessorRetryInformation(
+        ILogger logger,
+        string messageType,
+        long sequence,
+        string correlationId,
+        string partitionKey,
+        int nextRetryNumber,
+        TimeSpan retryDueTime,
+        DateTimeOffset nextRetryDateTime);
+
+    [LoggerMessage(
+            EventId = 10,
+            Level = LogLevel.Error,
+            Message = "Unhandled command processing error. Command handlers should throw application error exceptions. Processing command number {CommandSequence}, type '{CommandType}', correlationId '{CorrelationId}' for aggregate key '{PartitionKey}'.")]
+    private static partial void LogUnhandledCommandProcessingError(
+        ILogger logger,
+        Exception exception,
+        long commandSequence,
+        string commandType,
+        string partitionKey,
+        string correlationId);
+}
