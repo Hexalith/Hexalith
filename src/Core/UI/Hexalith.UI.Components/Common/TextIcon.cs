f201f41c8df04cc12fff2a4878d86f5d36f52372
Jérôme Piquot <jpiquot@itaneo.com>
2025-01-30 13:58:26 +0100

    Add WarehouseId support to construction site management
    This commit introduces the `WarehouseId` property and related functionality across various classes and components. Key updates include:
    
    - Addition of `WarehouseId` in `ConstructionSiteCommandHandlerHelper.cs` and `AddConstructionSite.cs`.
    - Modifications to `ConstructionSiteAdded` to capture warehouse information.
    - Updates to UI components like `ConstructionSiteDetailsViewModel` and `ConstructionSiteEditViewModel` for proper data binding.
    - Enhancements to Razor components to manage warehouse data effectively.
    - Localization files updated to support new warehouse-related strings.
    - Introduction of the `TextIcon` class for text-based icons in the UI.
    
    These changes improve the construction site management system by integrating warehouse capabilities and enhancing user experience.

---
+++ src/Core/UI/Hexalith.UI.Components/Common/TextIcon.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="TextIcon.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Hexalith.UI.Components.Common;
+
+using Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// Represents a text icon component.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="TextIcon"/> class.
+/// </remarks>
+/// <param name="character">The character to be displayed as an icon.</param>
+public class TextIcon(string character) : Icon(character, IconVariant.Regular, IconSize.Custom, $"<span style=\"padding: 0px; margin: 0px;width: 20px;font-size: 20px;color: var(--accent-fill-rest);\">{character}</span>")
+{
+}
