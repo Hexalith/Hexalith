dfa6681de5b037cc1043aa6557019df72589a793
Jérôme Piquot <jpiquot@itaneo.com>
2025-03-26 13:04:12 +0100

    update modules

--- src/Core/Application/Hexalith.Application.Modules/Applications/HexalithWebServerApplication.cs
+++ src/Core/Application/Hexalith.Application.Modules/Applications/HexalithWebServerApplication.cs
@@ -1,6 +1,6 @@
 ﻿// <copyright file="HexalithWebServerApplication.cs" company="ITANEO">
-//     Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved. Licensed under the MIT
-//     license. See LICENSE file in the project root for full license information.
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
 namespace Hexalith.Application.Modules.Applications;
